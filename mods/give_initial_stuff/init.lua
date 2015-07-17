baa39fe6a702fab4bae528c64c5e2198e50aa33f
rubenwardy <rubenwardy@gmail.com>
2015-07-17 18:52:11 +0100

    Fix typos

--- mods/give_initial_stuff/init.lua
+++ mods/give_initial_stuff/init.lua
@@ -2,9 +2,8 @@ function give_initial_stuff(player)
 	if minetest.setting_getbool("give_initial_stuff") then
 		minetest.log("action", "Giving initial stuff to player "..player:get_player_name())
 		player:get_inventory():add_item('main', 'default:pick_steel')
+		player:get_inventory():add_item('main', 'default:sword_steel')
 		player:get_inventory():add_item('main', 'default:torch 99')
-		player:get_inventory():add_item('main', 'default:axe_steel')
-		player:get_inventory():add_item('main', 'default:shovel_steel')
 		player:get_inventory():add_item('main', 'default:cobble 99')
 	end
 end
