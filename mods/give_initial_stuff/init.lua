4a713b4f70e936af90343a5beb9dd0217aa1601a
rubenwardy <rubenwardy@gmail.com>
2015-11-22 19:21:19 +0000

    Add items on respawn

--- mods/give_initial_stuff/init.lua
+++ mods/give_initial_stuff/init.lua
@@ -1,10 +1,14 @@
 function give_initial_stuff(player)
 	if minetest.setting_getbool("give_initial_stuff") then
 		minetest.log("action", "Giving initial stuff to player "..player:get_player_name())
-		player:get_inventory():add_item('main', 'default:pick_wood')
-		player:get_inventory():add_item('main', 'default:sword_wood')
-		player:get_inventory():add_item('main', 'default:torch 3')
+		local inv = player:get_inventory()
+		inv:set_list("main", {})
+		inv:set_list("craft", {})
+		inv:add_item('main', 'default:pick_wood')
+		inv:add_item('main', 'default:sword_wood')
+		inv:add_item('main', 'default:torch 3')
 	end
 end
 
 minetest.register_on_newplayer(give_initial_stuff)
+minetest.register_on_respawnplayer(give_initial_stuff)
