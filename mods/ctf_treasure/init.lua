6995373b5a13827b9e3561415856840d96e53524
rubenwardy <rubenwardy@gmail.com>
2015-11-23 20:05:32 +0000

    Remove throwing

--- mods/ctf_treasure/init.lua
+++ mods/ctf_treasure/init.lua
@@ -15,7 +15,7 @@ treasurer.register_treasure("shooter:shotgun",0.05,2,1)
 treasurer.register_treasure("shooter:machine_gun",0.01,2,1)
 treasurer.register_treasure("shooter:ammo",0.5,2,{1,10})
 
-
+--[[
 treasurer.register_treasure("throwing:arrow_steel",0.1,5,{1,12})
 treasurer.register_treasure("throwing:arrow_stone",0.2,5,{1,12})
 treasurer.register_treasure("throwing:arrow_obsidian",0.3,5,{1,8})
@@ -25,6 +25,7 @@ treasurer.register_treasure("throwing:arrow_steel",0.2,5,{1,12})
 treasurer.register_treasure("throwing:bow_wood",0.9,5,{1,12})
 treasurer.register_treasure("throwing:longbow",0.4,5,1)
 treasurer.register_treasure("throwing:bow_steel",0.1,5,1)
+]]
 
 treasurer.register_treasure("default:pick_steel",0.3,5,{1,10})
 treasurer.register_treasure("default:pick_mese",0.005,5,1)
