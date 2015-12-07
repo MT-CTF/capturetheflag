f556a1ae261a49a46085f87594ed0191c4f21cfb
rubenwardy <rubenwardy@gmail.com>
2015-12-07 23:08:40 +0000

    Remove grass abms and plantlike grass decorations

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -98,27 +98,6 @@ default.cool_lava_flowing = function(pos)
 	minetest.sound_play("default_cool_lava", {pos = pos, gain = 0.25})
 end
 
-minetest.register_abm({
-	nodenames = {"default:lava_flowing"},
-	neighbors = {"group:water"},
-	interval = 1,
-	chance = 1,
-	action = function(...)
-		default.cool_lava_flowing(...)
-	end,
-})
-
-minetest.register_abm({
-	nodenames = {"default:lava_source"},
-	neighbors = {"group:water"},
-	interval = 1,
-	chance = 1,
-	action = function(...)
-		default.cool_lava_source(...)
-	end,
-})
-
-
 --
 -- Papyrus and cactus growing
 --
@@ -277,47 +256,6 @@ minetest.register_abm({
 			-- Remove node
 			minetest.remove_node(p0)
 			nodeupdate(p0)
-		end
-	end
-})
-
-
---
--- Grass growing
---
-
-minetest.register_abm({
-	nodenames = {"default:dirt"},
-	interval = 2,
-	chance = 200,
-	action = function(pos, node)
-		local above = {x = pos.x, y = pos.y + 1, z = pos.z}
-		local name = minetest.get_node(above).name
-		local nodedef = minetest.registered_nodes[name]
-		if nodedef and (nodedef.sunlight_propagates or nodedef.paramtype == "light") and
-				nodedef.liquidtype == "none" and
-				(minetest.get_node_light(above) or 0) >= 13 then
-			if name == "default:snow" or name == "default:snowblock" then
-				minetest.set_node(pos, {name = "default:dirt_with_snow"})
-			else
-				minetest.set_node(pos, {name = "default:dirt_with_grass"})
-			end
-		end
-	end
-})
-
-minetest.register_abm({
-	nodenames = {"default:dirt_with_grass"},
-	interval = 2,
-	chance = 20,
-	action = function(pos, node)
-		local above = {x = pos.x, y = pos.y + 1, z = pos.z}
-		local name = minetest.get_node(above).name
-		local nodedef = minetest.registered_nodes[name]
-		if name ~= "ignore" and nodedef and not ((nodedef.sunlight_propagates or
-				nodedef.paramtype == "light") and
-				nodedef.liquidtype == "none") then
-			minetest.set_node(pos, {name = "default:dirt"})
 		end
 	end
 })
