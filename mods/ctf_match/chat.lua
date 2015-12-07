b7dc53779e25f380051316e9c0baf2cee037256d
rubenwardy <rubenwardy@gmail.com>
2015-12-07 23:54:22 +0000

    Add ctf_match priv

--- mods/ctf_match/chat.lua
+++ mods/ctf_match/chat.lua
@@ -1,7 +1,11 @@
+minetest.register_privilege("ctf_match", {
+	description = "can skip matches"
+})
+
 minetest.register_chatcommand("ctf_next", {
 	description = "Skip to the next match",
 	privs = {
-		ctf_admin = true
+		ctf_match = true
 	},
 	func = function(name, param)
 		ctf_match.next()
