a19631f41df7633b590eaebb4c317335de697c7c
rubenwardy <rubenwardy@gmail.com>
2015-12-08 00:24:46 +0000

    Add /ctf_respawn

--- mods/ctf_match/chat.lua
+++ mods/ctf_match/chat.lua
@@ -11,3 +11,27 @@ minetest.register_chatcommand("ctf_next", {
 		ctf_match.next()
 	end
 })
+
+minetest.register_chatcommand("ctf_respawn", {
+	description = "Respawn a player (clean inv, send to base)",
+	privs = {
+		ctf_team_mgr = true
+	},
+	func = function(name, param)
+		local tplayer = ctf.player_or_nil(param)
+		if tplayer then
+			local player = minetest.get_player_by_name(param)
+			if player then
+				ctf.move_to_spawn(param)
+				give_initial_stuff(player)
+				minetest.chat_send_player(param,
+					"You were sent back to base and your inventory wiped (by " .. name .. ")")
+				return true, "Moved player to spawn and wiped inventory."
+			else
+				return false, "Player is not online."
+			end
+		else
+			return false, "Player does not exist or is not in any teams."
+		end
+	end
+})
