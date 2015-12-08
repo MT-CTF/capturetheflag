11b1866e8849acbf45237d22855af5f24ce210d2
rubenwardy <rubenwardy@gmail.com>
2015-12-08 19:23:00 +0000

    Add apples, stone swords and crossbows to chests

--- mods/ctf_treasure/init.lua
+++ mods/ctf_treasure/init.lua
@@ -1,13 +1,17 @@
 treasurer.register_treasure("default:ladder",0.3,5,{1,20})
 treasurer.register_treasure("default:torch",0.3,5,{1,20})
 treasurer.register_treasure("default:cobble",0.3,5,{45,99})
+treasurer.register_treasure("default:apple",0.3,5,{1,8})
 
 treasurer.register_treasure("default:pick_steel",0.5,5,{1,10})
 treasurer.register_treasure("default:pick_mese",0.005,5,1)
-treasurer.register_treasure("default:sword_steel",0.8,5,{1,10})
+treasurer.register_treasure("default:sword_stone",0.7,5,{1,10})
+treasurer.register_treasure("default:sword_steel",0.4,5,{1,10})
 
+treasurer.register_treasure("shooter:crossbow",0.5,2,{1,5})
 treasurer.register_treasure("shooter:pistol",0.4,2,{1,5})
 treasurer.register_treasure("shooter:rifle",0.1,2,{1,2})
 treasurer.register_treasure("shooter:shotgun",0.05,2,1)
 treasurer.register_treasure("shooter:machine_gun",0.01,2,1)
 treasurer.register_treasure("shooter:ammo",0.3,2,{1,10})
+treasurer.register_treasure("shooter:arrow_white",0.5,2,{1,10})
