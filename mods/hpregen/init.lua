36e76e7924fb9b005f9c422e0917e9cb747c0316
rubenwardy <rubenwardy@gmail.com>
2016-01-02 14:08:40 +0000

    Add hp regen (1 hp per 10 seconds)

---
+++ mods/hpregen/init.lua
@@ -0,0 +1,20 @@
+local regen_interval = tonumber(minetest.setting_get("regen_interval"))
+if regen_interval <= 0 then
+	regen_interval = 10
+end
+local regen_amount = tonumber(minetest.setting_get("regen_amount"))
+if regen_amount <= 0 then
+	regen_amount = 1
+end
+
+local function regen_all()
+	for _, player in pairs(minetest.get_connected_players()) do
+		local newhp = player:get_hp() + regen_amount
+		if newhp > 20 then
+			newhp = 20
+		end
+		player:set_hp(newhp)
+	end
+	minetest.after(regen_interval, regen_all)
+end
+minetest.after(regen_interval, regen_all)
