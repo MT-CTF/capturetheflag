3cab7dc253787db7aa18172c41e63c04ab19879d
rubenwardy <rubenwardy@gmail.com>
2017-10-06 01:44:28 +0100

    Improve help tab

--- mods/ctf_inventory/init.lua
+++ mods/ctf_inventory/init.lua
@@ -1,18 +1,32 @@
 local items = {
-	"Tips",
+	minetest.colorize("#66a0ff", "Game Play"),
 	"",
-	"*  Capture an enemy's flag by walking up to it, punching it,",
-	"   then running and punching your team's flag.",
+	"*  Capture an enemy's flag by walking up to it, punching it, then",
+	"   running and punching your team's flag.",
 	"*  Look for guns, grenades and other resources in chests.",
 	"*  Guns can destroy blocks.",
-	"*  See the league table by typing /rankings",
-	"   See your position in it by typing /rankings me",
-	"   Get to the top by capturing lots of flags, and having a high K/D ratio.",
+	"*  Good swords do more damage than guns, but need to be used at close range.",
+	"*  If the match is getting boring, type /vote, then /yes to vote yes.",
+	"*  Use apples to replenish health.",
+	"",
+
+	minetest.colorize("#66a0ff", "Team Co-op"),
+	"",
 	"*  Your team has a chest near your flag.",
 	"   Be warned that other teams can steal from it.",
 	"*  Your team name is displayed in the top left.",
 	"   to talk with only your team, type: /t message",
-	"*  If the match is getting boring, type /vote, then /yes to vote yes.",
+	"",
+
+	minetest.colorize("#66a0ff", "Player Rankings"),
+	"",
+	"*  See the league table by typing /rankings",
+	"*  See your position in it by typing /rankings me",
+	"*  Get to the top by capturing lots of flags, and having a high K/D ratio.",
+	"",
+
+	minetest.colorize("#66a0ff", "Contact Moderators"),
+	"",
 	"*  Report people who sabotage using /report."
 }
 for i = 1, #items do
