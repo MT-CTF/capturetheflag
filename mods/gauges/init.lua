d1f21b62dbbbb9d86ab0fc5ac2a79ac4e80e778a
rubenwardy <rubenwardy@gmail.com>
2017-10-12 16:39:15 +0100

    Fix attempt for gauges disappearing

--- mods/gauges/init.lua
+++ mods/gauges/init.lua
@@ -10,34 +10,75 @@ local hp_bar = {
 	wielder = nil,
 }
 
+function vector.sqdist(a, b)
+	local dx = a.x - b.x
+	local dy = a.y - b.y
+	local dz = a.z - b.z
+	return dx*dx + dy*dy + dz*dz
+end
+
 function hp_bar:on_step(dtime)
-	local wielder = self.wielder
+	local wielder = self.wielder and minetest.get_player_by_name(self.wielder)
 	if wielder == nil then
-		self.object:remove()
-		 return
-	elseif minetest.env:get_player_by_name(wielder:get_player_name()) == nil then
+		print("[gauges] Gauge removed as null wielder! " .. dump(self.wielder))
 		self.object:remove()
 		return
 	end
-	hp = wielder:get_hp()
-	breath = wielder:get_breath()
-	self.object:set_properties({textures = {"health_" .. tostring(hp) .. ".png^breath_" .. tostring(breath) .. ".png",},}
-	)
+
+	if vector.sqdist(wielder:get_pos(), self.object:get_pos()) > 3 then
+		print("[gauges] Gauge removed as not attached! " .. dump(self.wielder))
+		self.object:remove()
+		return
+	end
+
+	local hp = wielder:get_hp()
+	local breath = wielder:get_breath()
+	self.object:set_properties({
+		textures = {
+			"health_" .. tostring(hp) .. ".png^breath_" .. tostring(breath) .. ".png",
+		},
+	})
 end
 
 minetest.register_entity("gauges:hp_bar", hp_bar)
 
-function add_HP_gauge(pl)
-		local pos = pl:getpos()
-		local ent = minetest.env:add_entity(pos, "gauges:hp_bar")
-		if ent ~= nil then
-			ent:set_attach(pl, "", {x = 0, y = 10, z = 0}, {x = 0, y = 0, z = 0})
-			ent = ent:get_luaentity()
-			ent.wielder = pl
-		end
+local function add_HP_gauge(name)
+	local player = minetest.get_player_by_name(name)
+	local pos = player:get_pos()
+	local ent = minetest.add_entity(pos, "gauges:hp_bar")
+	if ent ~= nil then
+		ent:set_attach(player, "", {x = 0, y = 10, z = 0}, {x = 0, y = 0, z = 0})
+		ent = ent:get_luaentity()
+		ent.wielder = player:get_player_name()
+	end
 end
 
-if minetest.setting_getbool("health_bars") ~= false -- “If not defined or set to true then”
-and minetest.setting_getbool("enable_damage") then -- Health bars only display when damage is enabled.
-	minetest.register_on_joinplayer(add_HP_gauge)
+-- If health_bars not defined or set to true
+if minetest.setting_getbool("health_bars") ~= false and
+		minetest.setting_getbool("enable_damage") then
+	minetest.register_on_joinplayer(function(player)
+		minetest.after(1, add_HP_gauge, player:get_player_name())
+	end)
 end
+
+local function check_gauges()
+	for _, player in pairs(minetest.get_connected_players()) do
+		local pname = player:get_player_name()
+		local found = false
+		local objects = minetest.get_objects_inside_radius(player:get_pos(), 1)
+		for _, object in pairs(objects) do
+			local le = object:get_luaentity()
+			if le and le.wielder == pname then
+				found = true
+				break
+			end
+		end
+
+		if not found then
+			print("Gauge not found!")
+			add_HP_gauge(pname)
+		end
+	end
+	minetest.after(9.3, check_gauges)
+end
+minetest.after(2, check_gauges)
