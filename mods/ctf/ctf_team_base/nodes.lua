303cc74b4b57799e828e72bb896556d0583be749
rubenwardy <rubenwardy@gmail.com>
2018-01-27 19:25:57 +0000

    Add reinforced cobble

--- mods/ctf/ctf_team_base/nodes.lua
+++ mods/ctf/ctf_team_base/nodes.lua
@@ -5,3 +5,17 @@ minetest.register_node("ctf_team_base:ind_cobble", {
 	groups = {immortal = 1},
 	sounds = default.node_sound_stone_defaults(),
 })
+
+minetest.register_node("ctf_team_base:reinforced_cobble", {
+	description = "Reinforced Cobblestone",
+	tiles = {"ctf_team_base_reinforced_cobble.png"},
+	is_ground_content = false,
+	groups = {cracky = 1, stone = 2},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_craft({
+	output = "ctf_team_base:reinforced_cobble",
+	type   = "shapeless",
+	recipe = {"default:cobble", "default:steel_ingot"},
+})
