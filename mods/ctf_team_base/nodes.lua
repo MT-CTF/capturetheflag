6c6f150b91597ddf8ded20321d8b1987b66fc8e7
rubenwardy <rubenwardy@gmail.com>
2018-01-27 19:12:08 +0000

    Split ctf_team_base init.lua into multiple files

---
+++ mods/ctf_team_base/nodes.lua
@@ -0,0 +1,7 @@
+minetest.register_node("ctf_team_base:ind_cobble", {
+	description = "Cobblestone",
+	tiles = {"default_cobble.png"},
+	is_ground_content = false,
+	groups = {immortal = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
