bae1530a20ffb67a0025330bb2dd4be668ac7157
Anand S <ClobberXD@gmail.com>
2018-08-05 19:12:08 +0530

    Minor fix

--- mods/playertag/init.lua
+++ mods/playertag/init.lua
@@ -5,7 +5,7 @@ local builtin_tags = {}
 ctf_flag.register_on_pick_up(function(attname, flag)
 	playertag.set(minetest.get_player_by_name(attname), playertag.TYPE_BUILTIN,
 			{ a=255, r=255, g=0, b=0 })
-	table.insert(builtin_tags, name)
+	table.insert(builtin_tags, attname)
 end)
 
 ctf_flag.register_on_drop(function(attname, flag)
