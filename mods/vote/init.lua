d9aa40f4cd9330bc2c8e58c8e1417653ffc62b2e
MinetestSam <42088654+MinetestSam@users.noreply.github.com>
2018-11-24 21:43:51 +0530

    Update init.lua

--- mods/vote/init.lua
+++ mods/vote/init.lua
@@ -339,12 +339,12 @@ if set == nil or minetest.is_yes(set) then
 						minetest.chat_send_all("Vote passed, " ..
 								#results.yes .. " to " .. #results.no .. ", " ..
 								self.name .. " will be kicked.")
-						minetest.kick_player(self.name, "The vote to kick you passed. You have been temporarily banned for 10 minutes")
+						minetest.kick_player(self.name, "The vote to kick you passed.\n You have been temporarily banned for 10 minutes.")
 						-- after player kick
-					vlist[self.name].locked = true
+						vlist[self.name].locked = true
 						minetest.after(600, function()
-					vlist[self.name] = nil
-					end)
+							vlist[self.name] = nil
+						end)
 					else
 						minetest.chat_send_all("Vote failed, " ..
 								#results.yes .. " to " .. #results.no .. ", " ..
