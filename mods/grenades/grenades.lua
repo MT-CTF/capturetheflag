7c4ba5cbff6c8a3b45ad1007096de8987e76f80c
LoneWolfHT <lonewolf04361@gmail.com>
2018-11-28 15:33:44 -0800

    Fix everything else

--- mods/grenades/grenades.lua
+++ mods/grenades/grenades.lua
@@ -1,184 +1,188 @@
 local settings = minetest.settings
 
-local regular = settings:get_bool("enable_regular_grenade")
-local flash = settings:get_bool("enable_flashbang_grenade")
-local smoke = settings:get_bool("enable_smoke_grenade")
+local regular = settings:get_bool("grenades.enable_regular")
+local flash = settings:get_bool("grenades.enable_flashbang")
+local smoke = settings:get_bool("grenades.enable_smoke")
 
 
 -- Regular Grenade
 
-if not regular or regular == true then
-    grenades.register_grenade("regular", {
-        description = "A regular grenade (Kills anyone near where it explodes)",
-        image = "grenades_regular.png",
-        on_explode = function(pos, name)
-            local player = minetest.get_player_by_name(name)
-
-            local radius = 4
-
-            minetest.add_particlespawner({
-                amount = 20,
-                time = 0.5,
-                minpos = vector.subtract(pos, radius),
-                maxpos = vector.add(pos, radius),
-                minvel = {x=0, y=5, z=0},
-                maxvel = {x=0, y=7, z=0},
-                minacc = {x=0, y=1, z=0},
-                maxacc = {x=0, y=1, z=0},
-                minexptime = 0.3,
-                maxexptime = 0.6,
-                minsize = 7,
-                maxsize = 10,
-                collisiondetection = true,
-                collision_removal = false,
-                vertical = false,
-                texture = "grenades_smoke.png",
-            })
-
-            for k, v in ipairs(minetest.get_objects_inside_radius(pos, radius)) do
-                if v:is_player() and v:get_hp() > 0 then
-                    v:punch(player, 2, {damage_groups = {fleshy = 22-vector.distance(pos, v:get_pos())}}, nil)
-                end
+if regular ~= false then
+	grenades.register_grenade("regular", {
+		description = "Regular grenade (Kills anyone near blast)",
+		image = "grenades_regular.png",
+		on_explode = function(pos, name)
+            if not name or not pos then
+                return
             end
-        end,
-    })
+
+			local player = minetest.get_player_by_name(name)
+
+			local radius = 6
+
+			minetest.add_particlespawner({
+				amount = 20,
+				time = 0.5,
+				minpos = vector.subtract(pos, radius),
+				maxpos = vector.add(pos, radius),
+				minvel = {x=0, y=5, z=0},
+				maxvel = {x=0, y=7, z=0},
+				minacc = {x=0, y=1, z=0},
+				maxacc = {x=0, y=1, z=0},
+				minexptime = 0.3,
+				maxexptime = 0.6,
+				minsize = 7,
+				maxsize = 10,
+				collisiondetection = true,
+				collision_removal = false,
+				vertical = false,
+				texture = "grenades_smoke.png",
+			})
+
+			for k, v in ipairs(minetest.get_objects_inside_radius(pos, radius)) do
+				if v:is_player() and v:get_hp() > 0 then
+					v:punch(player, 2, {damage_groups = {fleshy = 21.5-vector.distance(pos, v:get_pos())}}, nil)
+				end
+			end
+		end,
+	})
 end
 
 -- Flashbang Grenade
 
-if flash == true then
-    grenades.register_grenade("flashbang", {
-        description = "A flashbang grenade (Blinds all who look at the explosion)",
-        image = "grenades_flashbang.png",
-        on_explode = function(pos, name)
-            for k, v in ipairs(minetest.get_objects_inside_radius(pos, 15)) do
-                if v:is_player() and v:get_hp() > 0 then
-                    local playerdir = vector.round(v:get_look_dir())
-                    local grenadedir = vector.round(vector.direction(v:get_pos(), pos))
-                    local pname = v:get_player_name()
+if flash ~= false then
+	grenades.register_grenade("flashbang", {
+		description = "Flashbang grenade (Blinds all who look at blast)",
+		image = "grenades_flashbang.png",
+		on_explode = function(pos, name)
+			for k, v in ipairs(minetest.get_objects_inside_radius(pos, 15)) do
+				if v:is_player() and v:get_hp() > 0 then
+					local playerdir = vector.round(v:get_look_dir())
+					local grenadedir = vector.round(vector.direction(v:get_pos(), pos))
+					local pname = v:get_player_name()
 
-                    if playerdir.x == grenadedir.x and playerdir.z == grenadedir.z then
-                        for i = 1, 3, 1 do
-                            local key = v:hud_add({
-                                hud_elem_type = "image",
-                                position = {x=0, 0},
-                                name = "death_list_hud",
-                                scale = {x=1000, y=1000},
-                                text = "grenades_white_"..tostring(i)..".png",
-                                alignment = {x=0, y=0},
-                                offset = {x=0, y=0}
-                            })
+					if vector.equals(playerdir, grenadedir) then
+						for i = 0, 3, 1 do
+							local key = v:hud_add({
+								hud_elem_type = "image",
+								position = {x = 0, y = 0},
+								name = "flashbang hud "..pname,
+								scale = {x = -200, y = -200},
+								text = "grenades_white.png^[opacity:"..tostring(255-(i*13)),
+								alignment = {x = 0, y = 0},
+								offset = {x = 0, y = 0}
+							})
 
-                            minetest.after(1.6*i, function()
-                                if minetest.get_player_by_name(pname) then
-                                    minetest.get_player_by_name(pname):hud_remove(key)
-                                end
-                            end)
-                        end
-                    end
+							minetest.after(1.6 * i, function()
+								if minetest.get_player_by_name(pname) then
+									minetest.get_player_by_name(pname):hud_remove(key)
+								end
+							end)
+						end
+					end
 
-                end
-            end
-        end,
-    })
+				end
+			end
+		end,
+	})
 end
 
 -- Smoke Grenade
 
-if not smoke or smoke == true then
-    grenades.register_grenade("smoke", {
-        description = "A smoke grenade (Generates a lot of smoke around the detonation area)",
-        image = "grenades_smoke_grenade.png",
-        on_explode = function(pos, name)
-            for i = 0, 5, 1 do
-                minetest.add_particlespawner({
-                    amount = 100,
-                    time = 10,
-                    minpos = vector.subtract(pos, 3.5),
-                    maxpos = vector.add(pos, 3.5),
-                    minvel = {x=0, y=2, z=0},
-                    maxvel = {x=0, y=3, z=0},
-                    minacc = {x=1, y=0.2, z=1},
-                    maxacc = {x=1, y=0.2, z=1},
-                    minexptime = 0.3,
-                    maxexptime = 1,
-                    minsize = 100,
-                    maxsize = 100,
-                    collisiondetection = false,
-                    collision_removal = false,
-                    vertical = false,
-                    texture = "grenades_smoke.png",
-                })
-            end
-        end,
-        particle = {
-            image = "grenades_smoke.png",
-            life = 1,
-            size = 4,
-            glow = 0,
-            interval = 5,
-        }
-    })
+if smoke ~= false then
+	grenades.register_grenade("smoke", {
+		description = "Smoke grenade (Generates smoke around blast site)",
+		image = "grenades_smoke_grenade.png",
+		on_explode = function(pos, name)
+			for i = 0, 5, 1 do
+				minetest.add_particlespawner({
+					amount = 15,
+					time = 11,
+					minpos = vector.subtract(pos, 3.5),
+					maxpos = vector.add(pos, 3.5),
+					minvel = {x=0, y=2, z=0},
+					maxvel = {x=0, y=3, z=0},
+					minacc = {x=1, y=0.2, z=1},
+					maxacc = {x=1, y=0.2, z=1},
+					minexptime = 0.3,
+					maxexptime = 1,
+					minsize = 150,
+					maxsize = 150,
+					collisiondetection = false,
+					collision_removal = false,
+					vertical = false,
+					texture = "grenades_smoke.png",
+				})
+			end
+		end,
+		particle = {
+			image = "grenades_smoke.png",
+			life = 1,
+			size = 4,
+			glow = 0,
+			interval = 5,
+		}
+	})
 end
 
 --
 -- Crafts
 --
 
-if settings:get_bool("enable_grenade_recipes") == true then
+if settings:get_bool("grenades.enable_crafting") == true then
 
-    -- Regular Grenade
+	-- Regular Grenade
 
-    if not regular or regular == true then
-        minetest.register_craft({
-            type = "shaped",
-            output = "grenades:grenade_regular",
-            recipe = {
-                {"", "default:steel_ingot", ""},
-                {"default:steel_ingot", "default:coal_lump", "default:steel_ingot"},
-                {"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"}
-            },
-        })
-    end
+	if regular ~= false then
+		minetest.register_craft({
+			type = "shaped",
+			output = "grenades:grenade_regular",
+			recipe = {
+				{"", "default:steel_ingot", ""},
+				{"default:steel_ingot", "default:coal_lump", "default:steel_ingot"},
+				{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"}
+			},
+		})
+	end
 
-    -- Smoke Grenade
+	-- Smoke Grenade
 
-    if not smoke or smoke == true then
-        minetest.register_craft({
-            type = "shaped",
-            output = "grenades:grenade_smoke",
-            recipe = {
-                {"", "default:steel_ingot", ""},
-                {"default:steel_ingot", "grenades:gun_powder", "default:steel_ingot"},
-                {"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"}
-            }
-        })
-    end
+	if smoke ~= false then
+		minetest.register_craft({
+			type = "shaped",
+			output = "grenades:grenade_smoke",
+			recipe = {
+				{"", "default:steel_ingot", ""},
+				{"default:steel_ingot", "grenades:gun_powder", "default:steel_ingot"},
+				{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"}
+			}
+		})
+	end
 
-    --Flashbang Grenade
+	--Flashbang Grenade
 
-    if not flash or flash == true then
-        minetest.register_craft({
-            type = "shaped",
-            output = "grenades:grenade_flashbang",
-            recipe = {
-                {"", "default:steel_ingot", ""},
-                {"default:steel_ingot", "default:torch", "default:steel_ingot"},
-                {"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"}
-            },
-        })
-    end
+	if flash ~= false then
+		minetest.register_craft({
+			type = "shaped",
+			output = "grenades:grenade_flashbang",
+			recipe = {
+				{"", "default:steel_ingot", ""},
+				{"default:steel_ingot", "default:torch", "default:steel_ingot"},
+				{"default:steel_ingot", "default:steel_ingot", "default:steel_ingot"}
+			},
+		})
+	end
 
-    -- Other
+	-- Other
 
-    minetest.register_craftitem("grenades:gun_powder", {
-        description = "A dark powder used for crafting some grenades",
-        inventory_image = "grenades_gun_powder.png"
-    })
+	minetest.register_craftitem("grenades:gun_powder", {
+		description = "A dark powder used for crafting some grenades",
+		inventory_image = "grenades_gun_powder.png"
+	})
 
-    minetest.register_craft({
-        type = "shapeless",
-        output = "grenades:gun_powder",
-        recipe = {"default:coal_lump", "default:coal_lump", "default:coal_lump", "default:coal_lump"},
-    })
+	minetest.register_craft({
+		type = "shapeless",
+		output = "grenades:gun_powder",
+		recipe = {"default:coal_lump 4"},
+	})
 end
--- mods/grenades/init.lua
+++ mods/grenades/init.lua
@@ -1,127 +1,112 @@
 grenades = {}
 
 local function throw_grenade(name, player)
-    local dir = player:get_look_dir()
-    local pos = player:get_pos()
-    local obj = minetest.add_entity({x=pos.x+dir.x, y=pos.y+1.3, z=pos.z+dir.z}, name)
-    local yaw = player:get_look_yaw()
+	local dir = player:get_look_dir()
+	local pos = player:get_pos()
+	local obj = minetest.add_entity({x=pos.x+dir.x, y=pos.y+1.4, z=pos.z+dir.z}, name)
+	local yaw = player:get_look_yaw()
 
-    obj:setvelocity({x=dir.x * 16, y=dir.y * 16, z=dir.z * 16})
-    obj:setacceleration({x=dir.x * -3, y=-17, z=dir.z * -3})
-    obj:setyaw(yaw + math.pi)
+	obj:setvelocity({x=dir.x * 20, y=dir.y * 20, z=dir.z * 20})
+	obj:setacceleration({x=dir.x * -4, y=-20, z=dir.z * -4})
 
-    return(obj:get_luaentity())
+	return(obj:get_luaentity())
 end
 
 function grenades.register_grenade(name, def)
-    if not def.timeout then def.timeout = 5 end
+	if not def.timeout then
+		def.timeout = 5
+	end
 
-    local grenade_entity = {
-        physical = true,
-        timer = 0,
-        visual = "sprite",
-        visual_size = {x=1, y=1, z=1},
-        textures = {def.image},
-        collisionbox = {1, 1, 1, 1, 1, 1},
-        on_step = function(self, dtime)
-            local pos = self.object:getpos()
-            local node = minetest.get_node(pos)
-            local obj = self.object
+	local grenade_entity = {
+		physical = true,
+		timer = 0,
+		visual = "sprite",
+		visual_size = {x=1, y=1, z=1},
+		textures = {def.image},
+		collisionbox = {1, 1, 1, 1, 1, 1},
+		on_step = function(self, dtime)
+			local pos = self.object:getpos()
+			local node = minetest.get_node(pos)
+			local obj = self.object
 
-            if self.particle == nil then
-                self.particle = 0
-            end
+			if self.particle == nil then
+				self.particle = 0
+			end
 
-            if self.timer then
-                self.timer = self.timer + dtime
-            else
-                self.timer = dtime
-            end
+			if self.timer then
+				self.timer = self.timer + dtime
+			else
+				self.timer = dtime
+			end
 
-            if def.particle and self.particle >= 4 then
-                self.particle = 0
+			if def.particle and self.particle >= 4 then
+				self.particle = 0
 
-                minetest.add_particle({
-                    pos = obj:get_pos(),
-                    velocity = vector.divide(obj:get_velocity(), 2),
-                    acceleration = vector.divide(obj:get_acceleration(), -5),
-                    expirationtime = def.particle.life,
-                    size = def.particle.size,
-                    collisiondetection = true,
-                    collision_removal = true,
-                    vertical = false,
-                    texture = def.particle.image,
-                    glow = def.particle.glow
-                })
-            elseif def.particle and self.particle < def.particle.interval then
-                self.particle = self.particle + 1
-            end
+				minetest.add_particle({
+					pos = obj:get_pos(),
+					velocity = vector.divide(obj:get_velocity(), 2),
+					acceleration = vector.divide(obj:get_acceleration(), -5),
+					expirationtime = def.particle.life,
+					size = def.particle.size,
+					collisiondetection = true,
+					collision_removal = true,
+					vertical = false,
+					texture = def.particle.image,
+					glow = def.particle.glow
+				})
+			elseif def.particle and self.particle < def.particle.interval then
+				self.particle = self.particle + 1
+			end
 
-            if self.timer > def.timeout or node.name ~= "air" then
-                def.on_explode(pos, self.thrower_name)
+			if self.timer > def.timeout or node.name ~= "air" then
+				def.on_explode(pos, self.thrower_name)
 
-                obj:remove()
-            end
-        end
-    }
+				obj:remove()
+			end
+		end
+	}
 
-    minetest.register_entity("grenades:grenade_"..name, grenade_entity)
+	minetest.register_entity("grenades:grenade_"..name, grenade_entity)
 
-    if def.placeable == true then
-        minetest.register_node("grenades:grenade_"..name, {
-            description = def.description,
-            stack_max = 1,
-            range = 4,
-            paramtype = "light",
-            sunlight_propagates = true,
-            walkable = false,
-            drawtype = "plantlike",
-            selection_box = {
-                type = "fixed",
-                fixed = {-0.3, -0.5, -0.3, 0.3, 0.4, 0.3},
-            },
-            tiles = {def.image},
-            inventory_image = def.image,
-            groups = {oddly_breakable_by_hand = 2},
-            on_use = function(itemstack, user, pointed_thing)
-                local player_name = user:get_player_name()
+	local newdef = {}
 
-                if pointed_thing.type ~= "node" then
-                    local grenade = throw_grenade("grenades:grenade_"..name, user)
-                    grenade.timer = 0
-                    grenade.thrower_name = player_name
+	newdef.description = def.description
+	newdef.stack_max = 1
+	newdef.range = 4
+	newdef.inventory_image = def.image
+	newdef.on_use = function(itemstack, user, pointed_thing)
+		local player_name = user:get_player_name()
 
-                    if not minetest.setting_getbool("creative_mode") then
-                        itemstack = ""
-                    end
-                end
+		if pointed_thing.type ~= "node" then
+			local grenade = throw_grenade("grenades:grenade_"..name, user)
+			grenade.timer = 0
+			grenade.thrower_name = player_name
 
-                return itemstack
-            end
-        })
-    else
-        minetest.register_craftitem("grenades:grenade_"..name, {
-            description = def.description,
-            stack_max = 1,
-            range = 4,
-            inventory_image = def.image,
-            on_use = function(itemstack, user, pointed_thing)
-                local player_name = user:get_player_name()
+			if not minetest.settings:get_bool("creative_mode") then
+				itemstack = ""
+			end
+		end
 
-                if pointed_thing.type ~= "node" then
-                    local grenade = throw_grenade("grenades:grenade_"..name, user)
-                    grenade.timer = 0
-                    grenade.thrower_name = player_name
+		return itemstack
+	end
 
-                    if not minetest.setting_getbool("creative_mode") then
-                        itemstack = ""
-                    end
-                end
+	if def.placeable == true then
 
-                return itemstack
-            end
-        })
-    end
+		newdef.tiles = {def.image}
+		newdef.selection_box = {
+			type = "fixed",
+			fixed = {-0.3, -0.5, -0.3, 0.3, 0.4, 0.3},
+		}
+		newdef.groups = {oddly_breakable_by_hand = 2}
+		newdef.paramtype = "light"
+		newdef.sunlight_propagates = true
+		newdef.walkable = false
+		newdef.drawtype = "plantlike"
+
+		minetest.register_node("grenades:grenade_"..name, newdef)
+	else
+		minetest.register_craftitem("grenades:grenade_"..name, newdef)
+	end
 end
 
 dofile(minetest.get_modpath("grenades").."/grenades.lua")
