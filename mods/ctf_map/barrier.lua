678c86aef57a726acd33ea4371b7d38a7fb0354f
ClobberXD <ClobberXD@gmail.com>
2018-11-29 21:49:07 +0530

    Allow players to cross barrier if fly=true, interact=false

--- mods/ctf_map/barrier.lua
+++ mods/ctf_map/barrier.lua
@@ -249,10 +249,13 @@ if minetest.get_modpath("ctf") then
 			local name = player:get_player_name()
 			local tname = ctf.player(name).team
 			local pos = player:get_pos()
-			if tname and
-					(tname == "blue" and pos.z >= 0) or (tname == "red" and pos.z <= 0) then
-				minetest.chat_send_player(name, "Match hasn't started yet!")
-				ctf.move_to_spawn(name)
+			local privs = minetest.get_player_privs(name)
+			if tname and not privs.fly and privs.interact then
+				if (tname == "blue" and pos.z >= 0) or
+						(tname == "red" and pos.z <= 0) then
+					minetest.chat_send_player(name, "Match hasn't started yet!")
+					ctf.move_to_spawn(name)
+				end
 			end
 		end
 
