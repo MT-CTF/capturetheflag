62ceb094b81dfca1b168c83167b52b01556a8556
LoneWolfHT <lonewolf04361@gmail.com>
2018-11-29 08:55:51 -0800

    Increase gravity of grenades (Decrease range)

--- mods/grenades/init.lua
+++ mods/grenades/init.lua
@@ -6,7 +6,7 @@ local function throw_grenade(name, player)
 	local obj = minetest.add_entity({x = pos.x + dir.x, y = pos.y + 1.4, z = pos.z + dir.z}, name)
 
 	obj:set_velocity({x = dir.x * 40, y = dir.y * 40, z = dir.z * 40})
-	obj:set_acceleration({x = dir.x * -4, y = -10, z = dir.z * -4})
+	obj:set_acceleration({x = dir.x * -4, y = -20, z = dir.z * -4})
 
 	return(obj:get_luaentity())
 end
