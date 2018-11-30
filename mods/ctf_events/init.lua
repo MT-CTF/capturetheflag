6a9e833d52114de5174a9daf86200eb50a159930
Anand S <ClobberXD@gmail.com>
2018-11-30 15:07:34 +0530

    Clear kill list on new match
    Also remove a couple of debug messages

--- mods/ctf_events/init.lua
+++ mods/ctf_events/init.lua
@@ -146,14 +146,12 @@ function ctf_events.update(player)
 end
 
 function ctf_events.update_all()
-	print("Updating ctf_event logs for all players")
 	for _, player in pairs(minetest.get_connected_players()) do
 		ctf_events.update(player)
 	end
 end
 
 ctf.register_on_killedplayer(function(victim, killer, type)
-	print("Player killed, posting ctf_event")
 	ctf_events.post("kill_" .. type, killer, victim)
 	ctf_events.update_all()
 end)
@@ -162,7 +160,7 @@ minetest.register_on_joinplayer(function(player)
 	ctf_events.update(player)
 end)
 
-ctf.register_on_new_game(function()
+ctf_match.register_on_new_match(function()
 	ctf_events.events = {}
 	ctf_events.update_all()
 end)
