a15fae79205ad391aad796d54c3659d0f449d250
MinetestSam <42088654+MinetestSam@users.noreply.github.com>
2019-01-07 20:48:35 +0530

    Update init.lua

--- mods/ctf_crafting/init.lua
+++ mods/ctf_crafting/init.lua
@@ -78,6 +78,14 @@ crafting.register_recipe({
 
 crafting.register_recipe({
 	type   = "inv",
+	output = "default:furnace",
+	items  = { "default:desert_cobble 8" },
+	always_known = true,
+	level  = 1,
+})
+
+crafting.register_recipe({
+	type   = "inv",
 	output = "doors:door_steel",
 	items  = { "default:steel_ingot 6" },
 	always_known = true,
@@ -168,14 +176,6 @@ crafting.register_recipe({
 	type   = "inv",
 	output = "shooter:ammo 1",
 	items  = { "default:steel_ingot 3", "default:coal_lump 2" },
-	always_known = true,
-	level  = 1,
-})
-
-crafting.register_recipe({
-	type   = "inv",
-	output = "default:furnace",
-	items  = { "default:desert_cobble 8" },
 	always_known = true,
 	level  = 1,
 })
