3c57a71f203c7c3830e5de87ac55e111aaf32deb
rubenwardy <rw@rubenwardy.com>
2019-01-11 19:01:49 +0000

    Improve sorting of player score metric

--- mods/ctf/ctf_metrics/init.lua
+++ mods/ctf/ctf_metrics/init.lua
@@ -13,20 +13,18 @@ local function step()
 
 	local sum = 0
 	local avg = 0
-	local bins = { [50]=0, [100]=0, [200]=0, [5000]=0, rest=0 }
+	local bins = { r050=0, r200=0, r5000=0, rest=0 }
 	if #minetest.get_connected_players() > 0 then
 		for _, player in pairs(minetest.get_connected_players()) do
 			local total, _ = ctf_stats.player(player:get_player_name())
 			sum = sum + total.score
 
 			if total.score > 174000 then
-				bins[50] = bins[50] + 1
-			elseif total.score > 80000 then
-				bins[100] = bins[100] + 1
+				bins.r050 = bins.r050 + 1
 			elseif total.score > 10000 then
-				bins[200] = bins[200] + 1
+				bins.r200 = bins.r200 + 1
 			elseif total.score > 1000 then
-				bins[5000] = bins[5000] + 1
+				bins.r5000 = bins.r5000 + 1
 			else
 				bins.rest = bins.rest + 1
 			end
