45e30d8755cb36d6df53612b9e5c33f37f067d4c
Anand S <ClobberXD@gmail.com>
2019-03-07 10:54:48 +0530

    Remove unused MTG tree-growing code

--- mods/mtg/default/init.lua
+++ mods/mtg/default/init.lua
@@ -38,7 +38,6 @@ default.gui_survival_form = "size[8,8.5]"..
 local default_path = minetest.get_modpath("default")
 
 dofile(default_path.."/functions.lua")
-dofile(default_path.."/trees.lua")
 dofile(default_path.."/nodes.lua")
 dofile(default_path.."/torch.lua")
 dofile(default_path.."/tools.lua")
