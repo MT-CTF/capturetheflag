2b921afe14e2eed6dac8199db9275972b429b503
Thomas-AC-Dev <50278460+Thomas-AC-Dev@users.noreply.github.com>
2019-05-10 09:38:39 -0700

    Add more items to blacklist
    Adds dirt, and stone picks and swords to blacklist.

--- mods/ctf/ctf_map/chest.lua
+++ mods/ctf/ctf_map/chest.lua
@@ -3,7 +3,10 @@ local blacklist = {
 	"default:jungleleaves",
 	"default:pine_needles",
 	"default:acacia_leaves",
-	"default:aspen_leaves"
+	"default:aspen_leaves",
+        "default:dirt",
+        "default:sword_stone",
+        "default:pick_stone"
 }
 
 local function max(a, b)
