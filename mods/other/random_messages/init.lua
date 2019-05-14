0d04bfd081f912cb562a44227f11af08f6e35841
Thomas--S <Thomas--S@users.noreply.github.com>
2019-05-14 09:54:02 +0200

    Add random message for maps catalog (#414)

--- mods/other/random_messages/init.lua
+++ mods/other/random_messages/init.lua
@@ -71,7 +71,8 @@ function random_messages.read_messages()
 		"Use /r to check your score and rank, and /rankings to see the league tables.",
 		"Use bandages on team-mates to heal them by 3-4 HP if their health is below 15 HP.",
 		"Use /m to add a team marker at pointed location, that's visible only to team-mates.",
-		"Use /summary to check scores of the current match and the previous match."
+		"Use /summary to check scores of the current match and the previous match.",
+		"Use /maps to view the maps catalog. It also contains license info and attribution details."
 	}
 end
 
