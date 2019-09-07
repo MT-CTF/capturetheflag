2b395cdedcc262523a17d34e3f1c8cef7d2c6563
ANAND <ClobberXD@gmail.com>
2019-09-07 13:33:43 +0530

    update.sh: Don't pull ctf_pvp_engine

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 git pull &&
-cd mods/ctf_pvp_engine &&
-git pull origin master &&
-cd ../crafting &&
+cd mods/crafting &&
 git pull origin master &&
 cd ../ctf/ctf_map/maps &&
 git pull origin master &&
