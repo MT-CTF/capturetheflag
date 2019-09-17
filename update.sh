f25ac0aadf028559afd65768bb050add2358a233
ANAND <ClobberXD@gmail.com>
2019-09-17 23:41:28 +0530

    update.sh: Do it the *right* way

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
+# Update repo and submodules
 git pull &&
-cd mods/crafting &&
-git pull origin master &&
-cd ../ctf/ctf_map/maps &&
-git pull origin master &&
-cp ./*.png ../textures/ &&
-cd ../../../.. &&
-./build.sh ../games/capturetheflag
+git submodule update --init --recursive &&
+
+# Copy map screenshots to ctf_map/textures
+cd mods/ctf/ctf_map && cp maps/*.png textures/ &&
+
+# Pre-process `ctf.setting` calls
+cd ../../.. && ./build.sh ../games/capturetheflag
