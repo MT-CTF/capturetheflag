aab7334dff8106bc76f2632c6a7f51e01abc7e3d
Cahrs <55798443+Cahrs@users.noreply.github.com>
2019-09-30 11:22:19 -0400

    Add files via upload

--- mods/ctf/ctf_crafting/init.lua
+++ mods/ctf/ctf_crafting/init.lua
@@ -147,7 +147,7 @@ crafting.register_recipe({
 
 crafting.register_recipe({
 	type   = "inv",
-	output = "ctf_traps:spike 1",
+	output = "ctf_traps:spike 2",
 	items  = { "default:steel_ingot 5" },
 	always_known = true,
 	level  = 1,
@@ -156,7 +156,7 @@ crafting.register_recipe({
 crafting.register_recipe({
 	type   = "inv",
 	output = "ctf_traps:dirt 1",
-	items  = { "default:dirt 5", "default:coal_lump" },
+	items  = { "default:dirt 3", "default:coal_lump" },
 	always_known = true,
 	level  = 1,
 })
@@ -164,7 +164,7 @@ crafting.register_recipe({
 crafting.register_recipe({
 	type   = "inv",
 	output = "ctf_traps:cobble 1",
-	items  = { "default:cobble 5", "default:coal_lump" },
+	items  = { "default:cobble 3", "default:coal_lump" },
 	always_known = true,
 	level  = 1,
 })
@@ -172,15 +172,15 @@ crafting.register_recipe({
 crafting.register_recipe({
 	type   = "inv",
 	output = "ctf_traps:stone 1",
-	items  = { "default:stone 5", "default:coal_lump" },
+	items  = { "default:stone 3", "default:coal_lump" },
 	always_known = true,
 	level  = 1,
 })
 
 crafting.register_recipe({
 	type   = "inv",
-	output = "ctf_traps:damage_cobble 1",
-	items  = { "default:cobble", "default:coal_lump 4", "default:steel_ingot 4" },
+	output = "ctf_traps:damage_cobble 2",
+	items  = { "default:cobble", "default:coal_lump 5", "default:steel_ingot 5" },
 	always_known = true,
 	level  = 1,
 })
