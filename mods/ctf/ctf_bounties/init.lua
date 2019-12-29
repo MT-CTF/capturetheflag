86ed80d0565e2c762268fa5b1c0667dc8552cf5d
MinetestSam <samarth0036@gmail.com>
2019-12-29 11:52:08 +0530

    Colourise the name of victim and killer and show bounty points when killed

--- mods/ctf/ctf_bounties/init.lua
+++ mods/ctf/ctf_bounties/init.lua
@@ -94,8 +94,13 @@ ctf.register_on_killedplayer(function(victim, killer)
 	end
 	bountied_player = nil
 
-	local msg = killer .. " has killed " .. victim .. " and received the prize!"
-	minetest.log("action", msg)
+	local killer_color = ctf_colors.get_color(ctf.player(killer)).css
+	local victim_color = ctf_colors.get_color(ctf.player(victim)).css
+	local msg = minetest.colorize(killer_color, killer) ..
+				minetest.colorize("#fff326", " has killed ") ..
+				minetest.colorize(victim_color, victim) ..
+				minetest.colorize("#fff326", " and received " .. bounty_score .. " points!")
+	minetest.log("action", minetest.strip_colors(msg))
 	minetest.chat_send_all(msg)
 	hud_score.new(killer, {
 		name = "ctf_bounty:prize",
