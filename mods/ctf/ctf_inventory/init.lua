35e82472e208c31e3ce97d8907ff4c7a9fd18fbc
MinetestSam <42088654+MinetestSam@users.noreply.github.com>
2019-12-29 20:50:49 +0530

    ctf_inventory: Fix colorization of headings in "Help" tab (#546)

--- mods/ctf/ctf_inventory/init.lua
+++ mods/ctf/ctf_inventory/init.lua
@@ -1,5 +1,7 @@
+local color = "#66A0FF"
 local items = {
-	minetest.colorize("#66a0ff", "Game Play"),
+	"",
+	color .. "Game Play",
 	"",
 	"*  Capture an enemy's flag by walking up to it, punching it, then",
 	"   running and punching your team's flag.",
@@ -12,21 +14,21 @@ local items = {
 	"   killing 2 people for every death.",
 	"",
 
-	minetest.colorize("#66a0ff", "Team Co-op"),
+	color .. "Team Co-op",
 	"",
 	"*  Your team has a chest near your flag.",
 	"*  Your team name is displayed in the top left.",
 	"   to talk with only your team, type: /t message",
 	"",
 
-	minetest.colorize("#66a0ff", "Player Rankings"),
+	color .. "Player Rankings",
 	"",
 	"*  See the league table by typing /rankings",
 	"*  See your position in it by typing /rankings me",
 	"*  Get to the top by capturing lots of flags, and having a high K/D ratio.",
 	"",
 
-	minetest.colorize("#66a0ff", "Contact Moderators"),
+	color .. "Contact Moderators",
 	"",
 	"*  Report people who sabotage using /report."
 }
