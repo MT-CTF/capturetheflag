7108c6e3b573c1d45bc8a1f358c531cf8de9e68d
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-01-25 17:50:12 +0800

    Update init.lua

--- mods/ctf/ctf_flag/init.lua
+++ mods/ctf/ctf_flag/init.lua
@@ -73,6 +73,25 @@ function ctf.get_spawn(team)
 	end
 end
 
+-- Add minimum build range
+local old_is_protected = minetest.is_protected
+local r = ctf.setting("flag.nobuild_radius")
+local rs = r * r
+function minetest.is_protected(pos, name)
+	if r <= 0 or rs == 0 then
+		return old_is_protected(pos, name)
+	end
+
+	local flag, distSQ = ctf_flag.get_nearest(pos)
+	if flag and pos.y >= flag.y - 1 and distSQ < rs then
+		minetest.chat_send_player(name,
+			"Too close to the flag to build! Leave at least " .. r .. " blocks around the flag.")
+		return true
+	else
+		return old_is_protected(pos, name)
+	end
+end
+
 -- Play sound
 ctf_flag.register_on_pick_up(function(attname, flag)
 	local vteam = ctf.team(flag.team)
