9b7a87695f41f2803f3e76045647435695067115
ANAND <ClobberXD@gmail.com>
2020-04-14 20:56:58 +0530

    Automatically scope-out when not wielding the same item

--- mods/pvp/sniper_rifles/init.lua
+++ mods/pvp/sniper_rifles/init.lua
@@ -2,20 +2,37 @@
 -- Private data --
 ------------------
 
--- Keep track of players who are scoping in
+-- Keep track of players who are scoping in, and their wielded item
 local scoped = {}
+
+-- Timer for scope-check globalstep
+local timer = 0.2
 
 -------------
 -- Helpers --
 -------------
 
-local function show_scope(name, fov_mult)
-	minetest.get_player_by_name(name):set_fov(1 / fov_mult, true)
-	-- e.g. let default fov = 100, if fov_mult == 8, then final FOV = 1/8 * 100 = 12.5
+local function show_scope(name, item_name, fov_mult)
+	local player = minetest.get_player_by_name(name)
+	if not player then
+		return
+	end
+
+	scoped[name] = item_name
+	-- e.g. if fov_mult == 8, then FOV = 1/8 * current_FOV, a.k.a 8x zoom
+	player:set_fov(1 / fov_mult, true)
+	player:set_hud_flags({ wielditem = false })
 end
 
 local function hide_scope(name)
-	minetest.get_player_by_name(name):set_fov(0)
+	local player = minetest.get_player_by_name(name)
+	if not player then
+		return
+	end
+
+	scoped[name] = nil
+	player:set_fov(0)
+	player:set_hud_flags({ wielditem = true })
 end
 
 local function on_rclick(item, placer, pointed_thing)
@@ -26,15 +43,41 @@ local function on_rclick(item, placer, pointed_thing)
 	local name = placer:get_player_name()
 	if scoped[name] then
 		hide_scope(name)
-		scoped[name] = nil
 	else
 		-- Remove _loaded suffix added to item name by shooter
 		local item_name = item:get_name():gsub("_loaded", "")
 		local fov_mult = shooter.registered_weapons[item_name].fov_mult
-		show_scope(name, fov_mult)
-		scoped[name] = fov_mult
+		show_scope(name, item_name, fov_mult)
 	end
 end
+
+------------------
+-- Sccope-check --
+------------------
+
+-- Hide scope if currently wielded item is not the same item
+-- player wielded when scoping
+
+local time = 0
+minetest.register_globalstep(function(dtime)
+	time = time + dtime
+	if time < timer then
+		return
+	end
+
+	time = 0
+	for name, original_item in pairs(scoped) do
+		local player = minetest.get_player_by_name(name)
+		if not player then
+			scoped[name] = nil
+		else
+			local wielded_item = player:get_wielded_item()
+			if wielded_item ~= original_item then
+				hide_scope(name)
+			end
+		end
+	end
+end)
 
 ----------------------------
 -- Rifle registration API --
