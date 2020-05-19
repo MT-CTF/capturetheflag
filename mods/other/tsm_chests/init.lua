b9424644e4ef9bb8c7c540eb1fbd22c9c5f180ed
ANAND <ClobberXD@gmail.com>
2020-05-19 23:19:48 +0530

    MTG: Trim out the remaining bloat from default

--- mods/other/tsm_chests/init.lua
+++ mods/other/tsm_chests/init.lua
@@ -27,9 +27,6 @@ local non_ground_nodes = {
 
 local chest_formspec =
 	"size[8,9]" ..
-	default.gui_bg ..
-	default.gui_bg_img ..
-	default.gui_slots ..
 	"list[current_name;main;0,0.3;8,4;]" ..
 	"list[current_player;main;0,4.85;8,1;]" ..
 	"list[current_player;main;0,6.08;8,3;8]" ..
