1b0b766cadfa9fd2c486c16b203f990d0b723dce
LoneWolfHT <lonewolf04361@gmail.com>
2020-09-13 19:40:10 -0700

    Style fix

--- mods/pvp/anticoward/init.lua
+++ mods/pvp/anticoward/init.lua
@@ -1,7 +1,7 @@
 -- Capture The Flag mod: anticoward
 minetest.register_on_player_hpchange(function(player, hp_change, reason)
 	if reason.type == "fall" and player:get_hp() + hp_change <= 0 then
-		return (-player:get_hp())+1
+		return (-player:get_hp()) + 1
 	end
 
 	return hp_change
