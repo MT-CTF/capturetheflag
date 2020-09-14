a24cb9befa45f15f54cdd456ee40d78b43786b96
LoneWolfHT <lonewolf04361@gmail.com>
2020-09-13 19:09:03 -0700

    Make fall damage leave at least 1 hp

---
+++ mods/pvp/anticoward/init.lua
@@ -0,0 +1,7 @@
+minetest.register_on_player_hpchange(function(player, hp_change, reason)
+	if reason.type == "fall" and player:get_hp() + hp_change <= 0 then
+		return (-player:get_hp())+1
+	end
+
+	return hp_change
+end, true)
