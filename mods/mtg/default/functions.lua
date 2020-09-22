65652ec4054b56b48c6b80dcfc6d6a32db22153e
LoneWolfHT <lonewolf04361@gmail.com>
2020-09-22 13:16:25 -0700

    Revert "MTG: Trim out the remaining bloat from default (#639)"
    This reverts commit ec06b8eddeaba78d1d11b8bad1ffc8ef6935213f.

--- mods/mtg/default/functions.lua
+++ mods/mtg/default/functions.lua
@@ -131,6 +131,21 @@ end
 
 
 --
+-- Lavacooling
+--
+
+default.cool_lava = function(pos, node)
+	if node.name == "default:lava_source" then
+		minetest.set_node(pos, {name = "default:obsidian"})
+	else -- Lava flowing
+		minetest.set_node(pos, {name = "default:stone"})
+	end
+	minetest.sound_play("default_cool_lava",
+		{pos = pos, max_hear_distance = 16, gain = 0.25})
+end
+
+
+--
 -- Optimized helper to put all items in an inventory into a drops list
 --
 
@@ -146,6 +161,63 @@ function default.get_inventory_drops(pos, inventory, drops)
 	end
 end
 
+
+--
+-- Papyrus and cactus growing
+--
+
+-- Wrapping the functions in ABM action is necessary to make overriding them possible
+
+function default.grow_cactus(pos, node)
+	if node.param2 >= 4 then
+		return
+	end
+	pos.y = pos.y - 1
+	if minetest.get_item_group(minetest.get_node(pos).name, "sand") == 0 then
+		return
+	end
+	pos.y = pos.y + 1
+	local height = 0
+	while node.name == "default:cactus" and height < 4 do
+		height = height + 1
+		pos.y = pos.y + 1
+		node = minetest.get_node(pos)
+	end
+	if height == 4 or node.name ~= "air" then
+		return
+	end
+	if minetest.get_node_light(pos) < 13 then
+		return
+	end
+	minetest.set_node(pos, {name = "default:cactus"})
+	return true
+end
+
+function default.grow_papyrus(pos, node)
+	pos.y = pos.y - 1
+	local name = minetest.get_node(pos).name
+	if name ~= "default:dirt_with_grass" and name ~= "default:dirt" then
+		return
+	end
+	if not minetest.find_node_near(pos, 3, {"group:water"}) then
+		return
+	end
+	pos.y = pos.y + 1
+	local height = 0
+	while node.name == "default:papyrus" and height < 4 do
+		height = height + 1
+		pos.y = pos.y + 1
+		node = minetest.get_node(pos)
+	end
+	if height == 4 or node.name ~= "air" then
+		return
+	end
+	if minetest.get_node_light(pos) < 13 then
+		return
+	end
+	minetest.set_node(pos, {name = "default:papyrus"})
+	return true
+end
 
 --
 -- Dig upwards
