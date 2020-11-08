d171436b467552047f2385c656386463d7807671
LoneWolfHT <lonewolf04361@gmail.com>
2020-11-08 09:08:14 -0800

    Fix some bugs with knight damage

--- mods/ctf/ctf_classes/melee.lua
+++ mods/ctf/ctf_classes/melee.lua
@@ -1,5 +1,7 @@
 minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, tool_capabilities, dir, damage)
 	if tool_capabilities.damage_groups.nopunch then return end
+	if ctf_respawn_immunity.is_immune(player) then return true end
+
 	local class = ctf_classes.get(hitter)
 
 	if class.properties.melee_bonus and hitter:get_wielded_item():get_name():find("sword") then
@@ -10,7 +12,10 @@ minetest.register_on_punchplayer(function(player, hitter, time_from_last_punch, 
 		end
 
 		if hitter:get_hp() > 0 then
+			player:set_hp(player:get_hp() - damage)
 			player:punch(hitter, 1, {damage_groups = {fleshy = time_from_last_punch*2, nopunch = 1}}, dir)
+
+			return true
 		end
 	end
 end)
