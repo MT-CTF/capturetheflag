67550aa1a532baaf5604b35230bd254ab80c6997
Apelta <54854228+TSafa-23@users.noreply.github.com>
2020-12-23 16:35:40 -0600

    Various improvements to function and distance
    Improvements/changes to improve performance and readability suggested by Lone_Wolf.

--- mods/pvp/antisabotage/init.lua
+++ mods/pvp/antisabotage/init.lua
@@ -1,6 +1,6 @@
-function myFunc(pos, oldnode, digger) --this is your local neighborhood uwu. Say hello!
+function preventTeamGrief(pos, oldnode, digger) -- better function names, distance calculations, and message suggested by Lone_Wolf.
 	minetest.set_node(pos, oldnode)
-	minetest.chat_send_player(digger:get_player_name(), "You may not mine blocks under where your teammates are near!")
+	minetest.chat_send_player(digger:get_player_name(), "You can't mine blocks under your teammates!")
 	local playerinv = digger:get_inventory()
 	local stack = ItemStack(oldnode)
 	playerinv:remove_item("main", stack)
@@ -15,8 +15,8 @@ minetest.register_on_dignode(function(pos, oldnode, digger)
 		if name ~= digger:get_player_name() then
 			if ctf.players[name].team == ctf.players[digger:get_player_name()].team then
 				if math.floor(player_pos.y) == pos.y then
-					if math.floor(player_pos.x+1.5) >= pos.x and pos.x >= math.floor(player_pos.x-.5) and math.floor(player_pos.z+1.5) >= pos.z and pos.z >= math.floor(player_pos.z-.5) then
-						myFunc(pos, oldnode, digger)
+					if vector.distance(player_pos, pos) <= 1.5 then -- proper distance measurement uwu
+						preventTeamGrief(pos, oldnode, digger)
 					end
 				end
 			end
