4f73cb98d575c39bf153c809db9e4a2fff8db0c7
Apelta <54854228+TSafa-23@users.noreply.github.com>
2020-12-24 12:04:03 -0600

    Update with suggestions from Lone_Wolf
    Various performance optimizations and other issues suggested by Lone_Wolf

--- mods/pvp/antisabotage/init.lua
+++ mods/pvp/antisabotage/init.lua
@@ -1,6 +1,6 @@
-function preventTeamGrief(pos, oldnode, digger) -- better function names, distance calculations, and message suggested by Lone_Wolf.
+function preventTeamGrief(pos, oldnode, digger, dname) -- better function names, distance calculations, and message suggested by Lone_Wolf.
 	minetest.set_node(pos, oldnode)
-	minetest.chat_send_player(digger:get_player_name(), "You can't mine blocks under your teammates!")
+	minetest.chat_send_player(dname, "You can't mine blocks under your teammates!")
 	local playerinv = digger:get_inventory()
 	local stack = ItemStack(oldnode)
 	playerinv:remove_item("main", stack)
@@ -8,15 +8,15 @@ end
 
 minetest.register_on_dignode(function(pos, oldnode, digger)
 	if not digger:is_player() then return end
-	for _, player in ipairs(minetest.get_connected_players()) do
-		if not player:is_player() then return end
+	local dname = digger:get_player_name()
+	for _, player in pairs(minetest.get_connected_players()) do
 		local name = player:get_player_name()
 		local player_pos = player:get_pos()
-		if name ~= digger:get_player_name() then
-			if ctf.players[name].team == ctf.players[digger:get_player_name()].team then
+		if name ~= dname then
+			if ctf.players[name].team == ctf.players[dname].team then
 				if math.floor(player_pos.y) == pos.y then
 					if vector.distance(player_pos, pos) <= 1.5 then -- proper distance measurement uwu
-						preventTeamGrief(pos, oldnode, digger)
+						preventTeamGrief(pos, oldnode, digger, dname)
 					end
 				end
 			end
