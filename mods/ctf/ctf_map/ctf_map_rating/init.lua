d75bcb6a5a6a8f2147572cb5ce609ee5e22db08d
realOneplustwo <oneplustwoisyes@gmail.com>
2021-01-19 11:43:24 -0800

    Added random message to inform players on how to rate maps

--- mods/ctf/ctf_map/ctf_map_rating/init.lua
+++ mods/ctf/ctf_map/ctf_map_rating/init.lua
@@ -35,7 +35,7 @@ local function can_rate(name)
     local stats, _ = ctf_stats.player(name)
     if stats.score < score_requirement then
         return 2
-    elseif rate_cooldown[name][ctf_map.map] then
+    elseif rate_cooldown[name] and rate_cooldown[name][ctf_map.map.name] then
         return 1
     else
         return 0
@@ -43,11 +43,27 @@ local function can_rate(name)
 end
 
 local function rate(name, rating)
-    rated[ctf_map.map][name] = rating
-    rate_cooldown[name][ctf_map.map] = true -- add cooldown
+    if not rated[ctf_map.map.name] then
+        rated[ctf_map.map.name] = {}
+    end
+
+    rated[ctf_map.map.name][name] = rating
+
+    if not rating and rated[ctf_map.map.name] = {} then -- delete nested tables when they are empty
+        rated[ctf_map.map.name] = nil
+    end
+
+    if not rate_cooldown[name] then
+        rate_cooldown[name] = {}
+    end
+    
+    rate_cooldown[name][ctf_map.map.name] = true -- add cooldown
     minetest.after(rate_delay, function(mname, map)
         rate_cooldown[mname][map] = nil
-    end, name, ctf_map.map) -- get rid of cooldown after rate_delay
+        if rate_cooldown[mname] = {} then -- delete nested tables when they are empty
+            rate_cooldown[mname] = nil
+        end
+    end, name, ctf_map.map.name) -- get rid of cooldown after rate_delay
 end
 
 function ctf_map_rating.get_map_rating(map_name)
@@ -62,11 +78,11 @@ minetest.register_chatcommand("rate", {
     params = "[rating]",
     description = "Rate the current map. Ratings are 1 to 5. Run /rate with no parameter to cancel your rating.",
     func = function(name, param)
-        if minetest.get_player_by_name(name) == nil then
+        if not minetest.get_player_by_name(name) then
             return
         end
         local code = can_rate(name)
-        
+
         if code == 0 then
             if param == "" then
                 rate(name, nil)
@@ -74,19 +90,19 @@ minetest.register_chatcommand("rate", {
 
             local rating = tonumber(param)
 
-            if rating ~= nil and rating % 1 == 0 then -- check if rating is an integer
-                if rating >= 1 and rating <= 5 then 
+            if rating and rating % 1 == 0 then -- check if rating is an integer
+                if rating >= 1 and rating <= 5 then
                     rate(name, rating)
                 else
-                    minetest.chat_send_player(name, minetest.colorize("Rating must be between 1 and 5", error_color))
+                    minetest.chat_send_player(name, minetest.colorize(error_color, "Rating must be between 1 and 5"))
                 end
             else
-                minetest.chat_send_player(name, minetest.colorize("Rating must be an integer", error_color))
+                minetest.chat_send_player(name, minetest.colorize(error_color, "Rating must be an integer"))
             end
         elseif code == 1 then
-            minetest.chat_send_player(name, minetest.colorize("Your rating cooldown is still active", error_color))
+            minetest.chat_send_player(name, minetest.colorize(error_color, "Your rating cooldown is still active"))
         else
-            minetest.chat_send_player(name, minetest.colorize("You must have at least 500 score to rate a map", error_color))
+            minetest.chat_send_player(name, minetest.colorize(error_color, "You must have at least 500 score to rate a map"))
         end
     end,
 })
