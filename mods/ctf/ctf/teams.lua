dc5f4cd78f5cdcece98f255297c27654975675d5
AnthonyDe <anthonydeassisi@gmail.com>
2021-01-27 16:05:33 -0500

    Fixed accessing nil value for max_hp

--- mods/ctf/ctf/teams.lua
+++ mods/ctf/ctf/teams.lua
@@ -474,7 +474,7 @@ function ctf.reward_assists(victim, killer, reward, is_suicide)
 		for name, damage in pairs(kill_assists[victim]["players"]) do
 			local playerExists = minetest.get_player_by_name(name)
 			if playerExists and name ~= victim then
-				local playerHP_max = ctf_respawn_delay.players[victim].old_max-8
+				local playerHP_max = ctf_classes.__classes[playerExists:get_meta():to_table().fields["ctf_classes:class"]].properties.max_hp-8
 				local standard = 0
 				local percentofhelp = damage / playerHP_max
 				if name ~= killer then
