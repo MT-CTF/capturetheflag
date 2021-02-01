c1a304f8b17705a6ca8ae7a602ac25499ced2277
Toby1710 <69259430+Toby1710@users.noreply.github.com>
2021-02-01 18:43:46 +0200

    Update mods/ctf/ctf_map/ctf_map_core/barrier.lua
    Co-authored-by: Apelta <54854228+TSafa-23@users.noreply.github.com>

--- mods/ctf/ctf_map/ctf_map_core/barrier.lua
+++ mods/ctf/ctf_map/ctf_map_core/barrier.lua
@@ -222,8 +222,7 @@ if minetest.get_modpath("ctf") then
 		local tname = ctf.player(name).team
 		if tname and
 				(tname == "blue" and pos.z >= 0) or (tname == "red" and pos.z <= 0) then
-			minetest.chat_send_player(name,
-					minetest.colorize("#e32d2d", "Can't dig beyond the barrier!"))
+			minetest.chat_send_player(name, minetest.colorize("#e32d2d", "Can't dig beyond the barrier!"))
 			return true
 		else
 			return old_is_protected(pos, name, ...)
