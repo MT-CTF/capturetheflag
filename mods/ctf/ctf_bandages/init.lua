65cef0d51ab139a939d70e01f30d4d44e87c2fc6
LoneWolfHT <lonewolf04361@gmail.com>
2021-02-05 15:45:51 -0800

    Revert "Add kill assists (#755)"
    This reverts commit 49f4e6fd470b121af32092f7a19ef6190abf39e0.

--- mods/ctf/ctf_bandages/init.lua
+++ mods/ctf/ctf_bandages/init.lua
@@ -24,9 +24,7 @@ minetest.register_craftitem("ctf_bandages:bandage", {
 			local limit = ctf_bandages.heal_percent *
 				object:get_properties().hp_max
 			if hp > 0 and hp < limit then
-				local hp_add = math.random(3,4)
-				ctf.add_heal_assist(pname, hp_add)
-				hp = hp + hp_add
+				hp = hp + math.random(3,4)
 				if hp > limit then
 					hp = limit
 				end
--- mods/ctf/ctf_stats/init.lua
+++ mods/ctf/ctf_stats/init.lua
@@ -418,9 +418,28 @@ local function calculateKillReward(victim, killer, toolcaps)
 end
 
 ctf.register_on_killedplayer(function(victim, killer, _, toolcaps)
-	local reward = calculateKillReward(victim, killer, toolcaps)
-	reward = math.floor(reward * 100) / 100
-	ctf.reward_assists(victim, killer, reward, (victim == killer))
+	-- Suicide is not encouraged here at CTF
+	if victim == killer then
+		return
+	end
+	local main, match = ctf_stats.player(killer)
+	if main and match then
+		local reward = calculateKillReward(victim, killer, toolcaps)
+		main.kills  = main.kills  + 1
+		main.score  = main.score  + reward
+		match.kills = match.kills + 1
+		match.score = match.score + reward
+		match.kills_since_death = match.kills_since_death + 1
+		_needs_save = true
+
+		reward = math.floor(reward * 100) / 100
+
+		hud_score.new(killer, {
+			name = "ctf_stats:kill_score",
+			color = "0x00FF00",
+			value = reward
+		})
+	end
 end)
 
 minetest.register_on_dieplayer(function(player)
