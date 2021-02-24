e9487c6bde63a692c7d3b9245540b4a3fd2fed2f
MinetestSam <42088654+MinetestSam@users.noreply.github.com>
2021-02-24 22:23:07 +0530

    Consider captures as a requirement for pro players (#541)
    Co-authored-by: LoneWolfHT <lonewolf04361@gmail.com>

--- mods/ctf/ctf_stats/init.lua
+++ mods/ctf/ctf_stats/init.lua
@@ -207,7 +207,7 @@ end
 function ctf_stats.is_pro(name)
 	local stats = ctf_stats.player(name)
 	local kd = stats.kills / (stats.deaths == 0 and 1 or stats.deaths)
-	return stats.score >= 10000 and kd >= 1.5
+	return stats.score >= 10000 and kd >= 1.5 and stats.captures >= 10
 end
 
 ctf.register_on_join_team(function(name, tname, oldteam)
