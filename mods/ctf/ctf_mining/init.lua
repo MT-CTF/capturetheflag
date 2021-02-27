488ddc946ff415844eac31e6e57074d928609c42
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-02-27 13:50:44 +0800

    sorry some change forgot safe

--- mods/ctf/ctf_mining/init.lua
+++ mods/ctf/ctf_mining/init.lua
@@ -1,19 +1,12 @@
 minetest.register_on_dignode(function(pos, oldnode, digger)
-	print(dump(pos))
-	print(dump(oldnode))
-	print(digger)
 	if not digger:is_player() then
 		return
 	end
 	local nname = oldnode.name
 	local pname = digger:get_player_name()
 	local ndef = minetest.registered_nodes[nname]
-	print(dump(ndef.groups))
-	print(ndef.groups.ores)
 	if ndef and ndef.groups and ndef.groups.ores then
 		local main, match = ctf_stats.player(pname)
-		print(dump(main))
-		print(dump(match))
 		if main and match then
 			main.score  = main.score  + ndef.groups.ores
 			match.score = match.score + ndef.groups.ores
