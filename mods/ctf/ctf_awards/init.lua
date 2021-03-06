e2582a71232277c5b837b86066914c89b02133f5
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-03-06 12:39:37 +0800

    Add award: canary, bounty, kd10

--- mods/ctf/ctf_awards/init.lua
+++ mods/ctf/ctf_awards/init.lua
@@ -39,8 +39,62 @@ awards.register_award("gravitation", {
   difficulty = 1,
 })
 
+awards.register_award("canary", {
+  title = "Canary",
+  description = "be the first to die in a match",
+  icon = "heart.png",
+  difficulty = 1,
+})
+
+local canary = ""
 minetest.register_on_dieplayer(function(ObjectRef, reason)
-  if reason.type == "fall" then
-    awards.unlock(ObjectRef:get_player_name(), "gravitation")
+  name = ObjectRef:get_player_name()
+  if canary == "" then
+    canary = name
+    awards.unlock(name, "canary")
   end
+  if reason.type == "fall" then
+    awards.unlock(name, "gravitation")
+  end
+end)
+
+awards.register_award("bounty", {
+  title = "Bounty Hunter",
+  description = "Kill 50 bounties",
+  icon = "default_tool_mesesword.png",
+  difficulty = 1,
+})
+
+awards.register_award("kd10", {
+  title = "???",
+  description = "Get a K/D above 10",
+  icon = "grenades_boom.png",
+  difficulty = 1,
+})
+
+local _mt_loaded = false
+
+ctf.register_on_new_game(function()
+  canary = ""
+  if _mt_loaded then
+    for _, x in pairs(minetest.get_connected_players()) do
+      local name = x:get_player_name()
+      local STAT = ctf_stats.player(name)
+      local BKS = STAT.bounty_kills
+      if BKS > 50 then
+        awards.unlock(name, "bounty")
+      end
+      local KD = STAT.kills
+      if STAT.deaths > 1 then
+        KD = KD / STAT.deaths
+      end
+      if KD > 10 then
+        awards.unlock(name, "kd10")
+      end
+    end
+  end
+end)
+
+minetest.register_on_mods_loaded(function()
+  _mt_loaded = true
 end)
