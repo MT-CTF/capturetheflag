b81a14325d6ee4eb6bf114aa9acc456410c88e84
Toby1710 <69259430+DiamondPlane@users.noreply.github.com>
2021-03-17 09:09:23 +0200

    Update mods/ctf/ctf_chat/init.lua
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- mods/ctf/ctf_chat/init.lua
+++ mods/ctf/ctf_chat/init.lua
@@ -257,7 +257,7 @@ minetest.register_chatcommand("t", {
 			end
 
 			if param == "" then
-			minetest.chat_send_player(name, "You did not write anything, see /help t")
+			    minetest.chat_send_player(name, "You did not write anything, see /help t")
 			end
 
 			local tcolor = ctf_colors.get_color(ctf.player(name))
