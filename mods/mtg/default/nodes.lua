e7d6eb03430c1edb2eced26ec86bc04f57c7c9f8
Axaxax88 <74550391+Axaxax88@users.noreply.github.com>
2021-05-17 11:09:58 +0200

    Make clay do not drop clay lump

--- mods/mtg/default/nodes.lua
+++ mods/mtg/default/nodes.lua
@@ -578,7 +578,7 @@ minetest.register_node("default:clay", {
 	description = "Clay",
 	tiles = {"default_clay.png"},
 	groups = {crumbly = 3},
-	drop = 'default:clay_lump 4',
+	drop = 'default:clay',
 	sounds = default.node_sound_dirt_defaults(),
 })
 
