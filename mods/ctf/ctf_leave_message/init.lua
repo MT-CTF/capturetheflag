363021b1b04ea717b44b82b0bfdf997919c0e8f6
ksandr <65916671+ksandronline@users.noreply.github.com>
2021-07-04 22:00:58 +0300

    Update mods/ctf/ctf_leave_message/init.lua
    Co-authored-by: David Leal <halfpacho@gmail.com>

--- mods/ctf/ctf_leave_message/init.lua
+++ mods/ctf/ctf_leave_message/init.lua
@@ -1,17 +1,12 @@
-function ctf.send_leave_message(name, timed_out)
-	local player = ctf.player(name)
-
+function core.send_leave_message(player_name, timed_out)
+	local player = ctf.player(player_name)
 	local tcolor = ctf_colors.get_color(player).css
 
-	local announcement = "*** " ..  minetest.colorize(tcolor, name) .. " left the game."
+	local announcement = "*** " ..  minetest.colorize(tcolor, player_name) .. " left the game."
 	if timed_out then
 		announcement = announcement .. " (timed out)"
 	end
-	core.chat_send_all(announcement)
-end
-
-function core.send_leave_message()
-	return
+	minetest.chat_send_all(announcement)
 end
 
 core.register_on_leaveplayer(function(player, timed_out)
