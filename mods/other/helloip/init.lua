38873bf9bcb7675a8bd7d7cf1631ac8423efeb80
ksandr <ru@ksandr.online>
2021-07-24 11:04:51 +0300

    fix luacheck whitespace

--- mods/other/helloip/init.lua
+++ mods/other/helloip/init.lua
@@ -13,7 +13,7 @@ local ip2num = function(ip)
 	local i1 = string.find(ip,".",1,true)
 	local i2 = string.find(ip,".",i1+1,true)
 	local i3 = string.find(ip,".",i2+1,true)
-	local ip1 = tonumber(string.sub(ip,1,i1-1)) or 0 
+	local ip1 = tonumber(string.sub(ip,1,i1-1)) or 0
 	local ip2 = tonumber(string.sub(ip,i1+1,i2-1)) or 0
 	local ip3 = tonumber(string.sub(ip,i2+1,i3-1)) or 0
 	local ip4 = tonumber(string.sub(ip,i3+1)) or 0
@@ -25,28 +25,24 @@ local iplookup = function(ip) -- ip is as number, returns index of entry or 0
 	local i1=1;
 	local i3 = #helloip;
 	local step = 0
-	
 	while i3-i1>0 and step < 100 do
 		step = step + 1
 		local i2 = math.floor((i1+i3)/2)
-		
 		local iplow = helloip[i2][1];
 		local iphigh = helloip[i2][2];
 		if ip>=iplow and ip <=iphigh then entry = i2; break; end
 		if i3-i1 == 1 then break end
-		
 		if ip > iphigh then -- right
 			i1 = i2
 		else -- left
 			i3 = i2
 		end
 	end
-	
 	return entry
 end
 
 -- parse csv into db
-local step = 0; 
+local step = 0;
 local i = 0
 local i1,i2,i3
 
@@ -56,7 +52,6 @@ while i and step < 10^6 do
 	i2 = string.find(csv,",",i1+1)
 	i3 = string.find(csv, "\n", i2+1)
 	if not i3 then break end
-	
 	local ipmin = ip2num(string.sub(csv, i+2,i1-2))
 	local ipmax = ip2num(string.sub(csv, i1+2,i2-2))
 	local ccode = string.sub(csv, i2+2,i3-2);
@@ -64,14 +59,13 @@ while i and step < 10^6 do
 	i=i3
 end
 
--- load country codelist from ccode.txt 
+-- load country codelist from ccode.txt
 -- copied as text from https://en.wikipedia.org/wiki/ISO_3166-1
 
 local f = assert(io.open(modpath .. "/ccode.txt", "r"))
 local cc = f:read("*all");f:close()
 local ccodes = {}
-
-local step = 0; 
+local step = 0;
 local i = 0
 local i1,i2,i3
 
@@ -81,7 +75,6 @@ while i and step < 10^3 do
 	i2 = string.find(cc," ",i1+1)
 	i3 = string.find(cc, "\n", i1+1)
 	if not i3 then break end
-	
 	local cname = string.sub(cc, i+1,i1-1)
 	local ccode = string.sub(cc, i1+2,i2-1)
 	ccodes[ccode] = cname
@@ -89,7 +82,6 @@ while i and step < 10^3 do
 end
 
 function helloip.get_player_country(name)
-	-- local name = player:get_player_name();
 	local ip = minetest.get_player_ip(name);
 	local entry = iplookup(ip2num(ip))
 	if entry > 0 then
