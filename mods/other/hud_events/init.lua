faeba9a0d693ceaec22cb30e83a8da408d952438
savilli <78875209+savilli@users.noreply.github.com>
2021-11-30 20:49:13 +0100

    Fix a kill hud disappearance after a time

--- mods/other/hud_events/init.lua
+++ mods/other/hud_events/init.lua
@@ -2,7 +2,7 @@ hud_events = {}
 
 local hud = mhud.init()
 
-local HUD_SHOW_TIME = 4
+local HUD_SHOW_TIME = 3
 local HUD_SHOW_QUICK_TIME = 2
 local HUD_SHOW_NEXT_TIME = 1
 
@@ -18,40 +18,24 @@ local HUD_COLORS = {
 }
 
 local hud_queues = {}
-minetest.register_on_leaveplayer(function(player)
-	hud:clear(player)
-	hud_queues[player] = nil
-end)
+local quick_event_timer = {}
 
-local function show_hud_event(player, huddef)
+minetest.register_on_leaveplayer(function(player)
 	local pname = player:get_player_name()
 
-	if not hud:exists(player, "hud_event") then
-		hud:add(player, "hud_event", {
-			hud_elem_type = "text",
-			position = {x = 0.5, y = 0.5},
-			offset = {x = 0, y = 20},
-			alignment = {x = "center", y = "down"},
-			text = huddef.text,
-			color = huddef.color,
-		})
-	else
-		hud:change(player, "hud_event", {text = ""})
-
-		minetest.after(HUD_SHOW_NEXT_TIME, function()
-			player = minetest.get_player_by_name(pname)
-			if not player then return end
-
-			hud:change(player, "hud_event", {
-				text = huddef.text,
-				color = huddef.color
-			})
-		end)
+	if hud_queues[pname] then
+		hud_queues[pname].t:cancel()
+		hud_queues[pname] = nil
 	end
-end
 
-local quick_event_timer = {}
+	if quick_event_timer[pname] then
+		quick_event_timer[pname]:cancel()
+		quick_event_timer[pname] = nil
+	end
+end)
+
 local function show_quick_hud_event(player, huddef)
+	local pname = player:get_player_name()
 
 	if not hud:exists(player, "hud_event_quick") then
 		hud:add(player, "hud_event_quick", {
@@ -66,57 +50,53 @@ local function show_quick_hud_event(player, huddef)
 		hud:change(player, "hud_event_quick", {text = huddef.text, color = huddef.color})
 	end
 
-	quick_event_timer[player] = 0
+	if quick_event_timer[pname] then
+		quick_event_timer[pname].cancel()
+	end
+	quick_event_timer[pname] = minetest.after(HUD_SHOW_QUICK_TIME, function()
+		hud:remove(player, "hud_event_quick")
+	end)
 end
 
-local timer = 0
-minetest.register_globalstep(function(dtime)
-	timer = timer + dtime
+local function handle_hud_events(player)
+	local pname = player:get_player_name()
 
-	if timer >= 1 then
-		for player, time in pairs(quick_event_timer) do
-			time = time + timer
+	local huddef = table.remove(hud_queues[pname].e, 1)
 
-			if time >= HUD_SHOW_QUICK_TIME then
-				hud:remove(player, "hud_event_quick")
-				quick_event_timer[player] = nil
-			else
-				quick_event_timer[player] = time
-			end
-		end
+	if not hud:exists(player, "hud_event") then
+		hud:add(player, "hud_event", {
+			hud_elem_type = "text",
+			position = {x = 0.5, y = 0.5},
+			offset = {x = 0, y = 20},
+			alignment = {x = "center", y = "down"},
+			text = huddef.text,
+			color = huddef.color,
+		})
 
-		timer = 0
+	else
+		hud:change(player, "hud_event", {text = ""})
 	end
-end)
 
-local function handle_hud_events(pname)
-	local player = minetest.get_player_by_name(pname)
-	if not player or not hud_queues[pname] then return end
+	hud_queues[pname].t = minetest.after(HUD_SHOW_NEXT_TIME, function()
+		hud:change(player, "hud_event", {
+			text = huddef.text,
+			color = huddef.color
+		})
 
-	show_hud_event(player, table.remove(hud_queues[pname], 1))
-
-	minetest.after(HUD_SHOW_TIME, function()
-		player = minetest.get_player_by_name(pname)
-		if not player or not hud_queues[pname] then return end
-
-		if #hud_queues[pname] >= 1 then
-			handle_hud_events(pname)
-		else
-			hud:remove(player, "hud_event")
-			hud_queues[pname]._started = false
-		end
+		hud_queues[pname].t = minetest.after(HUD_SHOW_TIME, function()
+			if #hud_queues[pname].e >= 1 then
+				handle_hud_events(player)
+			else
+				hud:remove(player, "hud_event")
+				hud_queues[pname] = nil
+			end
+		end)
 	end)
 end
 
 function hud_events.new(player, def)
 	player = PlayerObj(player)
-
 	if not player then return end
-	local pname = player:get_player_name()
-
-	if not hud_queues[pname] then
-		hud_queues[pname] = {_started = false}
-	end
 
 	if type(def) == "string" then
 		def = {text = def}
@@ -131,13 +111,17 @@ function hud_events.new(player, def)
 	end
 
 	if not def.quick then
-		table.insert(hud_queues[pname], {text = def.text, color = def.color})
+		local pname = player:get_player_name()
 
-		if not hud_queues[pname]._started then
-			hud_queues[pname]._started = true
-			handle_hud_events(pname)
+		if not hud_queues[pname] then
+			hud_queues[pname] = {e = {}}
+		end
+		table.insert(hud_queues[pname].e, {text = def.text, color = def.color})
+
+		if not hud_queues[pname].t then
+			handle_hud_events(player)
 		end
 	else
-		show_quick_hud_event(pname, def)
+		show_quick_hud_event(player, def)
 	end
 end
