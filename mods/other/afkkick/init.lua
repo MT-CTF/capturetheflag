c0f709a6b3ae4f7c82fff347070136734d3930ca
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-12-19 07:36:13 +0800

    Translations: afkkick

--- mods/other/afkkick/init.lua
+++ mods/other/afkkick/init.lua
@@ -14,7 +14,9 @@ local WARN_TIME = 20
 local players = {}
 local checkTimer = 0
 
-minetest.register_privilege("canafk")
+minetest.register_privilege("canafk",{
+    description = S("Allow to AFK without being kicked"),
+})
 
 minetest.register_on_joinplayer(function(player)
 	local playerName = player:get_player_name()
@@ -67,9 +69,8 @@ minetest.register_globalstep(function(dtime)
 
 				--Warn player if he/she has less than WARN_TIME seconds to move or be kicked
 				if players[playerName]["lastAction"] + MAX_INACTIVE_TIME - WARN_TIME < currGameTime then
-					minetest.chat_send_player(playerName, minetest.colorize("#FF8C00", "Warning, you have " ..
-						tostring(players[playerName]["lastAction"] + MAX_INACTIVE_TIME - currGameTime + 1) ..
-						" seconds to move or be kicked"))
+					minetest.chat_send_player(playerName, minetest.colorize("#FF8C00", S("Warning, you have @1 seconds to move or be kicked",
+                    tostring(players[playerName]["lastAction"] + MAX_INACTIVE_TIME - currGameTime + 1))))
 				end
 			end
 		end
