f68a4c5d8a0dc904e89a9ed85ec599ab81f2fe05
savilli <78875209+savilli@users.noreply.github.com>
2022-01-03 16:42:31 +0100

    Restore stamina on respawn and on new match

--- mods/other/sprint/init.lua
+++ mods/other/sprint/init.lua
@@ -10,66 +10,56 @@ local MIN_SPRINT  = tonumber(minetest.settings:get("sprint_min")       or 0.5)
 
 local players = {}
 
-local function setSprinting(player, info, sprinting)
-	if info.sprinting ~= sprinting then
-		if sprinting then
-			physics.set(player:get_player_name(), "sprint:sprint", {
-				speed = MOD_WALK,
-				jump  = MOD_JUMP
-			})
-		else
-			physics.remove(player:get_player_name(), "sprint:sprint")
-		end
-		info.sprinting = sprinting
+local function setSprinting(player, sprinting)
+	if sprinting then
+		physics.set(player:get_player_name(), "sprint:sprint", {
+			speed = MOD_WALK,
+			jump  = MOD_JUMP
+		})
+	else
+		physics.remove(player:get_player_name(), "sprint:sprint")
+	end
+end
+
+local function updateHud(player, info)
+	local numBars = math.floor(20 * info.stamina / STAMINA_MAX)
+	if info.lastHudSendValue ~= numBars then
+		info.lastHudSendValue = numBars
+		player:hud_change(info.hud, "number", numBars)
 	end
 end
 
 minetest.register_globalstep(function(dtime)
 	for name, info in pairs(players) do
 		local player = minetest.get_player_by_name(name)
-		if not player then
-			players[name] = nil
+		--Check if the player should be sprinting
+		local controls = player:get_player_control()
+		local sprintRequested = controls.aux1 and controls.up
+
+		if sprintRequested and info.stamina > MIN_SPRINT then
+			if not info.sprinting then
+				info.sprinting = true
+				setSprinting(player, true)
+			end
 		else
-			-- ##?## You can enable recharging when the E key is pressed by
-			--       following these instructions.
-
-			--Check if the player should be sprinting
-			local controls = player:get_player_control()
-			local sprintRequested = controls.aux1 and controls.up
-			-- ##1## replace info.sprintRequested with info.sprinting
-			if sprintRequested ~= info.sprintRequested then
-				if sprintRequested and info.stamina > MIN_SPRINT then
-					setSprinting(player, info, true)
-				elseif not sprintRequested then
-					setSprinting(player, info, false)
-				end
-			end
-			info.sprintRequested = sprintRequested
-
-			local staminaChanged = false
 			if info.sprinting then
-				info.stamina = info.stamina - dtime
-				staminaChanged = true
-				if info.stamina <= 0 then
-					info.stamina = 0
-					setSprinting(player, info, false)
-				end
-
-			-- ##2## remove `not info.sprintRequested and`
-			elseif not info.sprintRequested and info.stamina < STAMINA_MAX then
-				info.stamina = info.stamina + dtime * HEAL_RATE
-				staminaChanged = true
-				if info.stamina > STAMINA_MAX then
-					info.stamina = STAMINA_MAX
-				end
+				info.sprinting = false
+				setSprinting(player, false)
 			end
+		end
 
-			if staminaChanged then
-				local numBars = math.floor(20 * info.stamina / STAMINA_MAX)
-				if info.lastHudSendValue ~= numBars then
-					info.lastHudSendValue = numBars
-					player:hud_change(info.hud, "number", numBars)
+		if sprintRequested then
+			if info.stamina > 0 then
+				info.stamina = info.stamina - dtime
+				if info.stamina < 0 then
+					info.stamina = 0
 				end
+				updateHud(player, info)
+			end
+		else
+			if info.stamina < STAMINA_MAX then
+				info.stamina = info.stamina + dtime * HEAL_RATE
+				updateHud(player, info)
 			end
 		end
 	end
@@ -79,7 +69,6 @@ minetest.register_on_joinplayer(function(player)
 	local info = {
 		sprinting       = false,       -- Is the player actually sprinting?
 		stamina         = STAMINA_MAX, -- integer, the stamina we have left
-		sprintRequested = false,       -- is the sprint key down / etc?
 	}
 
 	info.hud = player:hud_add({
@@ -95,8 +84,21 @@ minetest.register_on_joinplayer(function(player)
 	players[player:get_player_name()] = info
 end)
 
-minetest.register_on_respawnplayer(function(player)
-	players[player:get_player_name()].stamina = STAMINA_MAX
+ctf_api.register_on_respawnplayer(function(player)
+	local info = players[player:get_player_name()]
+	if info.stamina < STAMINA_MAX then
+		info.stamina = STAMINA_MAX
+		updateHud(player, info)
+	end
+end)
+
+ctf_api.register_on_new_match(function()
+	for name, info in pairs(players) do
+		if info.stamina < STAMINA_MAX then
+			info.stamina = STAMINA_MAX
+			updateHud(minetest.get_player_by_name(name), info)
+		end
+	end
 end)
 
 minetest.register_on_leaveplayer(function(player)
