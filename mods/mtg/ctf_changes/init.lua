8f358dba7379f05522e206c847028686b7bfb37d
LoneWolfHT <lonewolf04361@gmail.com>
2022-01-09 12:52:01 -0800

    Limit apple spam a little more

--- mods/mtg/ctf_changes/init.lua
+++ mods/mtg/ctf_changes/init.lua
@@ -104,7 +104,7 @@ end)
 minetest.override_item("default:apple", {
 	on_use = function(itemstack, user, ...)
 		if not COOLDOWN:get(user) then
-			COOLDOWN:set(user, 0.2)
+			COOLDOWN:set(user, 0.3)
 
 			return minetest.item_eat(3)(itemstack, user, ...)
 		end
