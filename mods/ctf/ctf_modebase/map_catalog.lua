956a2e47b082fa0026c4b399a0a33ab969252c45
savilli <78875209+savilli@users.noreply.github.com>
2022-01-24 21:56:32 +0100

    Fix map selection algo
    Don't select the same map twice in case of disabled maps

--- mods/ctf/ctf_modebase/map_catalog.lua
+++ mods/ctf/ctf_modebase/map_catalog.lua
@@ -25,65 +25,81 @@ assert(#ctf_modebase.map_catalog.maps > 0 or ctf_core.settings.server_mode == "m
 -- List of shuffled map indices, used in conjunction with random map selection
 local shuffled_order
 local shuffled_idx
+local used_maps
+local unused_maps
 
 -- Fisher-Yates-Savilli shuffling algorithm, used for shuffling map selection order
 -- Adapted from snippet provided in https://stackoverflow.com/a/35574006
 -- Improved to ensure that the first maps from current shuffled order differ
 -- from the last maps from previous shuffled order
 -- You can set the minimum distance between the same map using map_recurrence_threshold param
-local function shuffle_maps(previous_order, map_recurrence_threshold)
+local function shuffle_maps(map_recurrence_threshold)
 	local maps_count = #ctf_modebase.map_catalog.maps
 
 	map_recurrence_threshold = math.min(map_recurrence_threshold or 0, maps_count - 1)
 
-	if not previous_order then
-		map_recurrence_threshold = 0
-		previous_order = {}
+	if unused_maps then
+		assert(#used_maps > 0)
+	else
+		unused_maps = {}
+		used_maps = {}
 		for i = 1, maps_count do
-			previous_order[i] = i
+			unused_maps[i] = i
 		end
 	end
 
-	-- Reset shuffled_idx
-	shuffled_idx = 1
+	local unused_maps_count = #unused_maps
+	table.insert_all(unused_maps, used_maps)
 
 	-- Create table of ordered indices
 	shuffled_order = {}
 
-	-- At first select maps that don't intersect with the last maps from previous order
-	for i = 1, map_recurrence_threshold do
-		local j = math.random(1, maps_count - map_recurrence_threshold)
-		local k = maps_count - map_recurrence_threshold + i
-		shuffled_order[i] = previous_order[j]
-		previous_order[j] = previous_order[k]
+	-- Choose the right boundary of maps we can select
+	-- We can select all unused maps and maps that don't intersect with threshold
+	local right = math.max(unused_maps_count, maps_count - map_recurrence_threshold)
+
+	for left = 1, maps_count do
+		local idx = math.random(left, right)
+		table.insert(shuffled_order, unused_maps[idx])
+		unused_maps[idx] = unused_maps[left]
+
+		-- Move the right boundary if we were limited by threshold
+		if left + (maps_count - map_recurrence_threshold) > unused_maps_count and right < maps_count then
+			right = right + 1
+		end
 	end
 
-	-- Select remaining maps
-	for i = map_recurrence_threshold + 1, maps_count do
-		local j = math.random(1, maps_count - i + 1)
-		local k = maps_count - i + 1
-		shuffled_order[i] = previous_order[j]
-		previous_order[j] = previous_order[k]
-	end
+	-- Reset index and used maps
+	shuffled_idx = 1
+	used_maps = {}
+	unused_maps = {}
 end
 
-function ctf_modebase.map_catalog.select_map()
-	-- If shuffled_idx overflows, re-shuffle map selection order
-	if not shuffled_order or shuffled_idx > #shuffled_order then
-		shuffle_maps(shuffled_order, 3)
+function ctf_modebase.map_catalog.select_map(filter)
+	local idx
+	while true do
+		-- If shuffled_idx overflows, re-shuffle map selection order
+		if not shuffled_order or shuffled_idx > #shuffled_order then
+			shuffle_maps(3)
+		end
+
+		-- Get the real idx stored in table shuffled_order at index [shuffled_idx]
+		idx = shuffled_order[shuffled_idx]
+		shuffled_idx = shuffled_idx + 1
+
+		if not filter or filter(ctf_modebase.map_catalog.maps[idx]) then
+			break
+		end
+
+		table.insert(unused_maps, idx)
 	end
 
-	-- Get the real idx stored in table shuffled_order at index [shuffled_idx]
-	ctf_modebase.map_catalog.current_map = shuffled_order[shuffled_idx]
-	shuffled_idx = shuffled_idx + 1
+	table.insert(used_maps, idx)
+	ctf_modebase.map_catalog.current_map = idx
 end
 
 function ctf_modebase.map_catalog.select_map_for_mode(mode)
-	while true do
-		ctf_modebase.map_catalog.select_map()
-		local map = ctf_modebase.map_catalog.maps[ctf_modebase.map_catalog.current_map]
-		if not map.game_modes or table.indexof(map.game_modes, mode) ~= -1 then
-			break
-		end
-	end
+	ctf_modebase.map_catalog.select_map(function(map)
+		return not map.game_modes or table.indexof(map.game_modes, mode) ~= -1
+	end)
 end
