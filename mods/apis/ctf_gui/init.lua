2b405a947f7d2563dd55b6bb4530bbd915fd9c7d
LoneWolfHT <lonewolf04361@gmail.com>
2022-02-07 08:52:46 -0800

    Move gui dev function

--- mods/apis/ctf_gui/init.lua
+++ mods/apis/ctf_gui/init.lua
@@ -53,46 +53,6 @@ function ctf_gui.init()
 	end)
 end
 
-function ctf_gui.show_formspec_dev(player, formname, formspec, formcontext)
-	local filepath = minetest.get_worldpath().."/ctf_gui/"
-	local filename = filepath.."file_edit.txt"
-
-	minetest.mkdir(filepath)
-
-	local file, error = io.open(filename, "w")
-
-	assert(file, error)
-
-	file:write(formspec)
-
-	file:close()
-
-	local function interval()
-		if formspec:sub(1, 3) == "[f]" then
-			local result, form = pcall(loadstring(formspec:sub(4)), formcontext)
-			ctf_gui.show_formspec(player, formname, result and form or "")
-		else
-			ctf_gui.show_formspec(player, formname, formspec)
-		end
-
-		minetest.after(1, function()
-			local f = io.open(filename, "r")
-
-			formspec = f:read("*a")
-
-			f:close()
-
-			if formspec ~= "exit" then
-				interval()
-			else
-				minetest.request_shutdown("Formspec dev requested shutdown", true)
-			end
-		end)
-	end
-
-	interval()
-end
-
 function ctf_gui.show_formspec(player, formname, formspec, formcontext)
 	player = PlayerName(player)
 
@@ -340,3 +300,5 @@ function ctf_gui.old_show_formspec(player, formname, formdef)
 
 	minetest.show_formspec(player, formdef.formname, formspec)
 end
+
+dofile(minetest.get_modpath("ctf_gui").."/dev.lua")
