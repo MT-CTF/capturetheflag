df09c107b7c9b33bb2927c037d41136208564b68
LoneWolfHT <lonewolf04361@gmail.com>
2022-02-12 14:23:51 -0800

    Undo core->minetest change

--- mods/ctf/ctf_rankings/init.lua
+++ mods/ctf/ctf_rankings/init.lua
@@ -10,7 +10,7 @@ if backend == "redis" then
 	local old_require = require
 
 	env.rawset(_G, "require", env.require)
-	rankings = env.dofile(env.minetest.get_modpath(env.minetest.get_current_modname()).."/redis.lua")
+	rankings = env.dofile(env.core.get_modpath(env.core.get_current_modname()).."/redis.lua")
 	env.rawset(_G, "require", old_require)
 else
 	rankings = ctf_core.include_files(backend..".lua")
