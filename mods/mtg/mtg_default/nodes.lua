847e3d49a7df555ad937357de0fe2e6d1a897396
Anthony-De <48418309+Anthony-De@users.noreply.github.com>
2022-03-17 09:10:31 -0400

    remove changes

--- mods/mtg/mtg_default/nodes.lua
+++ mods/mtg/mtg_default/nodes.lua
@@ -791,8 +791,8 @@ minetest.register_node("default:apple", {
 	on_use = minetest.item_eat(2),
 	sounds = default.node_sound_leaves_defaults(),
 
-	on_place = function()
-		return nil
+	after_place_node = function(pos, placer, itemstack)
+		minetest.set_node(pos, {name = "default:apple", param2 = 1})
 	end,
 
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
