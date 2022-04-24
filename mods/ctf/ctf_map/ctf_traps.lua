446e7d2c5dbfc7aec2fd6230594ea325e3dca158
Xenon <36995016+xenonca@users.noreply.github.com>
2022-04-24 21:18:26 +0200

    Add landmine

--- mods/ctf/ctf_map/ctf_traps.lua
+++ mods/ctf/ctf_map/ctf_traps.lua
@@ -101,3 +101,98 @@ minetest.register_node("ctf_map:reinforced_cobble", {
 	groups = {cracky = 1, stone = 2},
 	sounds = default.node_sound_stone_defaults(),
 })
+
+minetest.register_node("ctf_map:landmine", {
+	description = "Landmine",
+	drawtype = "nodebox",
+	tiles = {"ctf_map_landmine.png", "ctf_map_landmine.png^[transformFY"},
+	inventory_image = "ctf_map_landmine.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	groups = {cracky=1, level=2},
+	node_box = {
+		type = "fixed",
+		fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.5, -0.5, -0.5, 0.5, -0.4, 0.5},
+	},
+	after_place_node = function(pos, placer, itemstack, pointed_thing)
+		local meta = minetest.get_meta(pos)
+		local name = placer:get_player_name()
+
+		meta:set_string("placer", minetest.serialize({
+			team = ctf_teams.get(name),
+			name = name,
+		}))
+	end
+})
+
+minetest.register_abm({
+	label = "Landmine",
+	nodenames = {"ctf_map:landmine"},
+	interval = 1,
+	chance = 1,
+	action = function(pos, node, active_object_count, active_object_count_wider)
+		local objs = minetest.get_objects_in_area({x=pos.x-0.5, y=pos.y-0.5, z=pos.z-0.5}, {x=pos.x+0.5, y=pos.y-0.3, z=pos.z+0.5})
+		if #objs == 0 then
+			return
+		else
+			local plyrs = minetest.get_objects_inside_radius(pos, 3)
+			local meta = minetest.get_meta(pos)
+			local placer = minetest.deserialize(meta:get_string("placer"))
+			local placer_obj = placer and minetest.get_player_by_name(placer.name)
+
+			minetest.add_particlespawner({
+				amount = 20,
+				time = 0.5,
+				minpos = vector.subtract(pos, 3),
+				maxpos = vector.add(pos, 3),
+				minvel = {x = 0, y = 5, z = 0},
+				maxvel = {x = 0, y = 7, z = 0},
+				minacc = {x = 0, y = 1, z = 0},
+				maxacc = {x = 0, y = 1, z = 0},
+				minexptime = 0.3,
+				maxexptime = 0.6,
+				minsize = 7,
+				maxsize = 10,
+				collisiondetection = true,
+				collision_removal = false,
+				vertical = false,
+				texture = "grenades_smoke.png",
+			})
+	
+			minetest.add_particle({
+				pos = pos,
+				velocity = {x=0, y=0, z=0},
+				acceleration = {x=0, y=0, z=0},
+				expirationtime = 0.3,
+				size = 15,
+				collisiondetection = false,
+				collision_removal = false,
+				object_collision = false,
+				vertical = false,
+				texture = "grenades_boom.png",
+				glow = 10
+			})
+
+			minetest.sound_play("grenades_explode", {
+				pos = pos,
+				gain = 1.0,
+				max_hear_distance = 64,
+			})
+
+			for _, v in pairs(plyrs) do
+				if placer_obj then
+					v:punch(placer_obj, 1, {damage_groups = {fleshy = 10}}, nil)
+				else
+					local chp = v:get_hp()
+					v:set_hp(chp - 10)
+				end
+			end
+			minetest.remove_node(pos)
+		end
+	end
+})
