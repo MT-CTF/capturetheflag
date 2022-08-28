96df6586801901c87794b813975c004c44899605
LoneWolfHT <lonewolf04361@gmail.com>
2022-08-28 07:25:13 -0700

    Update init.lua

--- mods/ctf/ctf_chat/init.lua
+++ mods/ctf/ctf_chat/init.lua
@@ -28,7 +28,7 @@ minetest.override_chatcommand("msg", {
 		minetest.chat_send_player(sendto, str)
 
 		-- Make the sender-side message
-		str = "To " .. sendto .. ": " .. message
+		str = "Message sent to " .. sendto .. ": " .. message
 
 		minetest.log("action", string.format("[CHAT] PM from %s to %s: %s", name, sendto, message))
 		
