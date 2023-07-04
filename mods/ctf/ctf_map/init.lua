23780f1fd79e02140be71ad55d0fc744af393c0d
Peace12345 <102005519+165135@users.noreply.github.com>
2023-07-04 13:32:26 -0700

    Update init.lua

--- mods/ctf/ctf_map/init.lua
+++ mods/ctf/ctf_map/init.lua
@@ -72,7 +72,7 @@ ctf_core.include_files(
 local directory = minetest.get_modpath(minetest.get_current_modname()) .. "/maps/"
 function ctf_map.run_map_init(map)
     for _, entry in ipairs(minetest.get_dir_list(directory, true)) do
-        if entry == map.foldername then
+        if entry == map.dirname then
             for _, filename in ipairs(minetest.get_dir_list(directory .. "/" .. entry .. "/", false)) do
                 if filename == "init.lua" then
                     dofile(directory .. "/" .. entry .. "/"..filename)
