ba6691612b1c2d1c1f7ef7057db8f1c696e9b799
Sasha Richalim <sasha.richalim.che@gmail.com>
2023-07-09 00:19:00 +0530

    Fix case of not being pointed

--- mods/ctf/ctf_modebase/markers.lua
+++ mods/ctf/ctf_modebase/markers.lua
@@ -162,11 +162,10 @@ local function marker_func(name, param, specific_player, hpmarker)
 	if pointed and pointed.type == "object" and pointed.ref == player then
 		pointed = ray:next()
   end
-  if (vector.distance(pointed.under or pointed.ref:get_pos(), player:get_pos()) <= 2) and
+  if pointed and (vector.distance(pointed.under or pointed.ref:get_pos(), player:get_pos()) <= 2) and
     (player:get_hp() < player:get_properties().hp_max) then
-		hpmarker = true
+	  hpmarker = true
 	end
-
   if hpmarker == true then
     local concat
     local player_hpr = string.format("HP=[%i]/[%i]", player:get_hp(), player:get_properties().hp_max)
