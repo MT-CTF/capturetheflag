2ad011b4be60580d1b5df0b8d66020e6fc4984b4
Peace12345 (Aka Brain_Juice) <102005519+165135@users.noreply.github.com>
2023-07-30 22:12:56 -0700

    Update bandage.lua

--- mods/ctf/ctf_combat/ctf_healing/bandage.lua
+++ mods/ctf/ctf_combat/ctf_healing/bandage.lua
@@ -51,7 +51,12 @@ function ctf_healing.register_bandage(name, def)
 
 			    local hp_add = math.random(def.heal_min or 3, def.heal_max or 4)
 
-			    hp = math.min(hp + hp_add, limit)
+				if hp + hp_add > limit then
+				    hp_add = limit - hp
+				    hp = limit
+			    else
+				    hp = hp + hp_add
+			    end
 
 			    local result = RunCallbacks(ctf_healing.registered_on_heal, player, object, hp_add)
 
