310414c0db7c2214bf9d3361fc1ab20d8deba265
lazylier <80552599+lazylier@users.noreply.github.com>
2023-08-02 04:48:08 -0400

    add score for helping the winning team

--- mods/ctf/ctf_modebase/features.lua
+++ mods/ctf/ctf_modebase/features.lua
@@ -423,6 +423,14 @@ return {
 			capture_reward = capture_reward + score
 		end
 
+		for teammate in pairs(ctf_teams.online_players[pteam].players) do
+			if teammate ~= pname then
+				local teammate_value = (recent_rankings.get(teammate)[pteam.."_score"] or 0) / (team_scores[pteam].score or 1)
+				local victory_bonus = capture_reward * teammate_value
+				recent_rankings.add(teammate, {score = victory_bonus}, true)
+			end
+		end
+
 		recent_rankings.add(pname, {score = capture_reward, flag_captures = #teamnames})
 
 		teams_left = teams_left - #teamnames
