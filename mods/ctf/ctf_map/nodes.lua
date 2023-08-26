89398512bbe22e06958509884e07bc8ba89d05e2
1F616EMO <root@1f616emo.xyz>
2023-08-26 08:27:39 +0800

    lava barrier light source

--- mods/ctf/ctf_map/nodes.lua
+++ mods/ctf/ctf_map/nodes.lua
@@ -72,7 +72,9 @@ minetest.register_node("ctf_map:ind_lava", {
 	description = "Indestructible Lava Barrier Stone",
 	groups = {immortal = 1},
 	tiles = {"ctf_map_ind_lava.png"},
-	is_ground_content = false
+	is_ground_content = false,
+	paramtype = "light",
+	light_source = default.LIGHT_MAX - 1,
 })
 ctf_map.barrier_nodes[minetest.get_content_id("ctf_map:ind_lava")] = minetest.get_content_id("default:lava_source")
 
