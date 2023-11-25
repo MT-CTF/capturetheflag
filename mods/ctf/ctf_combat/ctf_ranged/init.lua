5daab62e06744e41bda76d89916812314e738892
LoneWolfHT <lonewolf04361@gmail.com>
2023-11-25 09:25:27 -0800

    Fix mis-placement of sound_play

--- mods/ctf/ctf_combat/ctf_ranged/init.lua
+++ mods/ctf/ctf_combat/ctf_ranged/init.lua
@@ -46,8 +46,9 @@ local function process_ray(ray, user, look_dir, def)
 						size = 1,
 						collisiondetection = false,
 						texture = "ctf_ranged_bullethole.png",
-						minetest.sound_play("ctf_ranged_ricochet", {pos = hitpoint.intersection_point})
 					})
+
+					minetest.sound_play("ctf_ranged_ricochet", {pos = hitpoint.intersection_point})
 				elseif nodedef.groups.liquid then
 					minetest.add_particlespawner({
 						amount = 10,
