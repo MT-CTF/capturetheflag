1be250c8649a0dc5085ae1481885bf74ef517f76
GreenBlob <lightboxfacts@gmail.com>
2023-11-28 09:58:47 +0800

    fix luacheck warning

--- mods/ctf/ctf_map/ctf_traps.lua
+++ mods/ctf/ctf_map/ctf_traps.lua
@@ -182,7 +182,7 @@ minetest.register_node("ctf_map:reinforced_cobble", {
 		local digger_team = ctf_teams.get(digger)
 		if placer_team ~= digger_team then
 			minetest.set_node(pos, {name = "ctf_map:reinforced_cobble_hardened"})
-			local meta = minetest.get_meta(pos)
+			meta = minetest.get_meta(pos)
 			meta:set_string("placer_team", placer_team)
 		end
 	end,
@@ -211,7 +211,7 @@ minetest.register_node("ctf_map:reinforced_cobble_hardened", {
 		local digger_team = ctf_teams.get(digger)
 		if placer_team == digger_team then
 			minetest.set_node(pos, {name = "ctf_map:reinforced_cobble"})
-			local meta = minetest.get_meta(pos)
+			meta = minetest.get_meta(pos)
 			meta:set_string("placer_team", placer_team)
 		end
 	end,
