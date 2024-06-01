dd3c04963b400b0139407ee038d180437f324b13
s20 <src4026@proton.me>
2024-06-01 19:57:23 +0530

    Oops, forgot to return false

--- mods/other/email/init.lua
+++ mods/other/email/init.lua
@@ -246,8 +246,7 @@ minetest.register_chatcommand("mail", {
 					minetest.check_player_privs(to, "basic_privs") then
 				return email.send_mail(name, to, msg)
 			else
-				minetest.chat_send_player(name,
-					"-!- Because you don't have the permission to speak, you can only mail staff members.")
+				return false, S("-!- Because you don't have the permission to speak, you can only mail staff members.")
 			end
 		else
 			return false, S("Usage: mail <playername> <some message>")
