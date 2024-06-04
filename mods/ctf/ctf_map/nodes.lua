7abc3d24c8903fb67d1bc4bada04fd4d3296333c
pl608 <78400949+pl608@users.noreply.github.com>
2024-06-04 13:08:04 -0700

    Make indestructible blocks only deal damage near enemy flags  (#1290)
    * initial update
    
    * commit to make it actually work
    
    * remove whitespaces
    
    * fixed more luacheck stuff
    
    * Update nodes.lua
    
    ---------
    
    Co-authored-by: LoneWolfHT <lonewolf04361@gmail.com>

--- mods/ctf/ctf_map/nodes.lua
+++ mods/ctf/ctf_map/nodes.lua
@@ -126,6 +126,22 @@ local function make_immortal(def)
 	def.description = def.description and ("Indestructible " .. def.description)
 end
 
+minetest.register_on_player_hpchange(function(player, hp_change, reason)
+	local pos = player:get_pos()
+	if reason.type == 'node_damage' and minetest.registered_nodes[reason.node].groups.immortal then
+		for _, flagteam in ipairs(ctf_teams.current_team_list) do
+			if flagteam ~= ctf_teams.get(player) then
+				local fdist = vector.distance(pos, ctf_map.current_map.teams[flagteam].flag_pos)
+				if fdist <= 6 then
+					return hp_change
+				end
+			end
+		end
+	end
+
+	return 0
+end, true)
+
 local queue = {}
 for name, def in pairs(minetest.registered_nodes) do
 	local mod, nodename = name:match"(..-):(.+)"
