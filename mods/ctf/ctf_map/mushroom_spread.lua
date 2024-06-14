053aa7d3dabfbb54d11e17b4d8362929e77bbd85
Farooq Karimi Zadeh <fkz@riseup.net>
2024-06-14 22:51:49 +0330

    add mushroom farming

---
+++ mods/ctf/ctf_map/mushroom_spread.lua
@@ -0,0 +1,45 @@
+local mushroom_globalstep_counter = 0.0
+local MUSHROOM_COUNTER_THRESHOLD = 1.0
+
+minetest.register_globalstep(function(dtime)
+	if not ctf_map.current_map then
+		return
+	end
+	mushroom_globalstep_counter = mushroom_globalstep_counter + dtime
+	if mushroom_globalstep_counter < MUSHROOM_COUNTER_THRESHOLD then
+		return
+	end
+	mushroom_globalstep_counter = 0.0
+	local pos1 = ctf_map.current_map.pos1
+	local pos2 = ctf_map.current_map.pos2
+	local x = math.random(pos1.x, pos2.x)
+	local y = math.random(pos1.y, pos2.y)
+	local z = math.random(pos1.z, pos2.z)
+	local half_x = math.floor(math.abs(pos1.x - pos2.x) / 5)
+	local half_y = math.floor(math.abs(pos1.y - pos2.y) / 5)
+	local half_z = math.floor(math.abs(pos1.z - pos2.z) / 5)
+	local positions = minetest.find_nodes_in_area_under_air(
+		{ x = x - half_x, y  = y - half_y, z = z - half_z},
+		{ x = x + half_x, y = y + half_y, z = z + half_z},
+		"group:soil"
+	)
+	for _idx, position in ipairs(positions) do
+		position = {
+			x = position.x,
+			y = position.y + 1,
+			z = position.z
+		}
+		if minetest.get_node_light(position) <= 3 then
+			local r = math.random()
+			if r >= 0.3 then
+				if r <= 0.65 then
+					minetest.set_node(position, { name = "flowers:mushroom_brown" })
+				else
+					minetest.set_node(position, { name = "flowers:mushroom_red" })
+				end
+			end
+		else
+			minetest.debug("Light too much!")
+		end
+	end
+end)
