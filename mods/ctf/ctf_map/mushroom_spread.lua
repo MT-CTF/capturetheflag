c587f08654db446daf229a1285e875da4c402dd4
Farooq Karimi Zadeh <fkz@riseup.net>
2024-06-18 12:38:40 +0330

    remove debug message, tweaak probabilities and params

--- mods/ctf/ctf_map/mushroom_spread.lua
+++ mods/ctf/ctf_map/mushroom_spread.lua
@@ -16,8 +16,8 @@ minetest.register_globalstep(function(dtime)
 	local y = math.random(pos1.y, pos2.y)
 	local z = math.random(pos1.z, pos2.z)
 	local half_x = math.floor(math.abs(pos1.x - pos2.x) / 4)
-	local half_y = math.floor(math.abs(pos1.y - pos2.y) / 4)
-	local half_z = math.floor(math.abs(pos1.z - pos2.z) / 2)
+	local half_y = math.floor(math.abs(pos1.y - pos2.y) / 2)
+	local half_z = math.floor(math.abs(pos1.z - pos2.z) / 4)
 	local positions = minetest.find_nodes_in_area_under_air(
 		{ x = x - half_x, y  = y - half_y, z = z - half_z},
 		{ x = x + half_x, y = y + half_y, z = z + half_z},
@@ -31,15 +31,13 @@ minetest.register_globalstep(function(dtime)
 		}
 		if minetest.get_node_light(position) <= 3 then
 			local r = math.random()
-			if r >= 0.4 then
-				if r <= 0.6 then
+			if r >= 0.5 then
+				if r <= 0.65 then
 					minetest.set_node(position, { name = "flowers:mushroom_brown" })
 				else
 					minetest.set_node(position, { name = "flowers:mushroom_red" })
 				end
 			end
-		else
-			minetest.debug("Light too much!")
 		end
 	end
 end)
