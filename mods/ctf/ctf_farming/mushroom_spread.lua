abb50c24adb74152d1c9a51315bd96cb2b562df5
Farooq Karimi Zadeh <fkz@riseup.net>
2024-11-03 21:55:13 +0330

    tweaks for mushroom farming

--- mods/ctf/ctf_farming/mushroom_spread.lua
+++ mods/ctf/ctf_farming/mushroom_spread.lua
@@ -1,5 +1,5 @@
 local mushroom_globalstep_counter = 0.0
-local MUSHROOM_COUNTER_THRESHOLD = 5.0
+local MUSHROOM_COUNTER_THRESHOLD = 4.0
 
 minetest.register_globalstep(function(dtime)
 	if not ctf_map.current_map then
@@ -25,7 +25,7 @@ minetest.register_globalstep(function(dtime)
 	-- --Farooq
 	--]]
 	local half_x = math.floor(math.abs(pos1.x - pos2.x) / 4)
-	local half_y = math.floor(math.abs(pos1.y - pos2.y) / 2)
+	local half_y = math.floor(math.abs(pos1.y - pos2.y) / 4)
 	local half_z = math.floor(math.abs(pos1.z - pos2.z) / 4)
 	local positions = minetest.find_nodes_in_area_under_air(
 		{ x = x - half_x, y  = y - half_y, z = z - half_z},
@@ -40,8 +40,8 @@ minetest.register_globalstep(function(dtime)
 		}
 		if minetest.get_node_light(position) <= 4 then
 			local r = math.random()
-			if r >= 0.5 then
-				if r <= 0.7 then
+			if r >= 0.4 then
+				if r >= 0.7 then
 					minetest.set_node(position, { name = "flowers:mushroom_brown" })
 				else
 					minetest.set_node(position, { name = "flowers:mushroom_red" })
