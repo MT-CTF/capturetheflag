f1216b8ecbbe3b93c3bfac7803eaec60abbf781a
LandarVargan <lonewolf04361@gmail.com>
2025-05-16 09:49:41 -0700

    Update nodes.lua

--- mods/ctf/ctf_map/nodes.lua
+++ mods/ctf/ctf_map/nodes.lua
@@ -105,7 +105,7 @@ minetest.register_node("ctf_map:ind_poison_water", {
 	groups = {immortal = 1},
 	sounds = default.node_sound_glass_defaults()
 })
-ctf_map.barrier_nodes[minetest.get_content_id("ctf_map:ind_poison_water")] = 
+ctf_map.barrier_nodes[minetest.get_content_id("ctf_map:ind_poison_water")] =
 		minetest.get_content_id("poison_water:poisonous_water")
 
 minetest.register_node("ctf_map:ind_lava", {
