ef134093551ef942a3fe98fceac942043af4d3fd
LoneWolfHT <lonewolf04361@gmail.com>
2025-08-09 15:02:21 -0700

    Allow players with noclip privs to noclip through flag base blocks

--- mods/ctf/ctf_map/nodes.lua
+++ mods/ctf/ctf_map/nodes.lua
@@ -175,7 +175,8 @@ minetest.register_on_player_hpchange(function(player, hp_change, reason)
 
 	if reason.type == 'node_damage' and def.groups.immortal and def.drawtype == "normal" and def.walkable ~= false then
 		for _, flagteam in ipairs(ctf_teams.current_team_list) do
-			if flagteam ~= ctf_teams.get(player) and ctf_map.current_map.teams[flagteam] then
+			if flagteam ~= ctf_teams.get(player) and ctf_map.current_map.teams[flagteam] and
+			not core.get_player_privs(player:get_player_name()).noclip == true then
 				local fdist = vector.distance(pos, ctf_map.current_map.teams[flagteam].flag_pos)
 				if fdist <= 6 then
 					return hp_change
