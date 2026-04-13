f9b9a977be905443fee7c661f362ca8c1d0e7c2d
Sasha <src4026@proton.me>
2026-04-13 15:12:32 +0530

    Undo formatting changes by local editor

--- mods/ctf/ctf_modebase/init.lua
+++ mods/ctf/ctf_modebase/init.lua
@@ -1,43 +1,43 @@
 ctf_modebase = {
 	-- Table containing all registered modes and their definitions
-	modes                         = {}, ---@type table
+	modes                = {},    ---@type table
 
 	-- Same as ctf_modebase.modes but in list form.
 	--
 	-- Exception: Disabled modes that show up in ctf_modebase.modes won't show up in the modelist
-	modelist                      = {}, ---@type list
+	modelist             = {},    ---@type list
 
 	-- Name of the mode currently being played. On server start this will be false
-	current_mode                  = false, ---@type string
+	current_mode         = false, ---@type string
 
 	-- Players can hit, heal, etc
-	match_started                 = false, ---@type boolean
+	match_started         = false, ---@type boolean
 
 	-- For team allocator
-	in_game                       = false, ---@type boolean
+	in_game               = false, ---@type boolean
 
 	-- Get the mode def of the current mode. On server start this will return false
-	get_current_mode              = function(self)
+	get_current_mode = function(self)
 		return self.current_mode and self.modes[self.current_mode]
 	end,
 
 	-- Amount of matches played since this mode won the last vote
-	current_mode_matches_played   = 0, ---@type integer
+	current_mode_matches_played = 0, ---@type integer
 
 	-- How many matches will be played for the current mode
-	current_mode_matches          = 5, ---@type integer
+	current_mode_matches        = 5, ---@type integer
 
 	-- taken_flags[Player Name] = list of team names
-	taken_flags                   = {},
+	taken_flags          = {},
 
 	-- team_flag_takers[Team name][Player Name] = list of team names
-	team_flag_takers              = {},
+	team_flag_takers     = {},
 
 	-- flag_taken[Team Name] = Name of thief
-	flag_taken                    = {},
+	flag_taken           = {},
 
 	--flag_captured[Team name] = true if captured, otherwise nil
-	flag_captured                 = {},
+	flag_captured        = {},
 
 	-- Choose who can see a player's nametag, defaults to their teammates
 	--
@@ -112,11 +112,7 @@ minetest.register_on_mods_loaded(function()
 			player:override_day_night_ratio(0)
 
 			player:set_hp(20)
-			player:set_nametag_attributes({
-				color = { a = 0, r = 255, g = 255, b = 255 },
-				text =
-				""
-			})
+			player:set_nametag_attributes({color = {a = 0, r = 255, g = 255, b = 255}, text = ""})
 		end)
 	elseif ctf_core.settings.server_mode == "play" then
 		ctf_modebase.start_new_match()
@@ -124,12 +120,11 @@ minetest.register_on_mods_loaded(function()
 
 	for _, name in pairs(ctf_modebase.modelist) do
 		if not ctf_modebase.modes[name].rounds then
-			ctf_settings.register("ctf_modebase:default_vote_" .. name, {
+			ctf_settings.register("ctf_modebase:default_vote_"..name, {
 				type = "list",
-				description = S("Match count vote for the mode") ..
-					" '" .. HumanReadable(name) .. "'",
-				list = { HumanReadable(name) .. " - Ask", "0", "1", "2", "3", "4", "5" },
-				_list_map = { "ask", 0, 1, 2, 3, 4, 5 },
+				description = S("Match count vote for the mode").." '"..HumanReadable(name).."'",
+				list = {HumanReadable(name).." - Ask", "0", "1", "2", "3", "4", "5"},
+				_list_map = {"ask", 0, 1, 2, 3, 4, 5},
 				default = "1", -- "Ask"
 			})
 		end
@@ -137,7 +132,7 @@ minetest.register_on_mods_loaded(function()
 end)
 
 minetest.override_chatcommand("pulverize", {
-	privs = { creative = true },
+	privs = {creative = true},
 })
 
 minetest.register_chatcommand("mode", {
