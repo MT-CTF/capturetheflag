018df5ea1584b1c8c0326865e3573c632173db11
LoneWolfHT <lonewolf04361@gmail.com>
2026-05-27 16:34:13 -0700

    Remove hudbars mod

--- mods/apis/ctf_settings/global_settings.lua
+++ mods/apis/ctf_settings/global_settings.lua
@@ -7,17 +7,3 @@ ctf_settings.register("ctf_kill_list:tp_size", {
 	image_scale_map = {2, 1, 0.5, 0.25, 0.125, 0.0625},
 	default = "2",
 })
-
-ctf_settings.register("use_hudbars", {
-	label = "Use hudbars instead of icon hud",
-	type = "bool",
-	default = "false",
-	description = "Use a bar with a label instead of icons for quantites like\n" ..
-		"health, stamina, and breath.\n" ..
-		"Disconnect and reconnect to see effects.",
-	on_change = function(player, new_value)
-		minetest.chat_send_player(player:get_player_name(), minetest.colorize("cyan",
-			"[Notice]: You need to rejoin for your HUD to change"
-		))
-	end,
-})
